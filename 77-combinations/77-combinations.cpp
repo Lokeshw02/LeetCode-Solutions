51602834e4e968c88432f3c767afc5f212d0fe74
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-02-07 16:55:37 +0530

    Time: 16 ms (92.86%), Space: 18 MB (35.41%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    void helper(int startIdx ,int n, int k, vector<int>&currSet, vector<vector<int>>&res){
+        
+        //Base Case 
+        if(k==0) {
+            res.push_back(currSet) ;
+            return ;
+        }
+        
+        if(startIdx>n) return ;
+        
+        if(n-startIdx+1 < k) return ;
+        
+        //Exlcude
+        
+        helper(startIdx +1,n,k,currSet,res) ;
+        
+        //Include 
+        
+        currSet.push_back(startIdx) ;
+        
+        helper(startIdx+1,n ,k-1,currSet,res) ;
+        
+        currSet.pop_back() ;
+        
+        
+        
+    }
+    
+    
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>res ;
+        vector<int>currSet ;
+        
+        helper(1,n,k,currSet,res) ;
+        
+        return res ;
+    }
+};

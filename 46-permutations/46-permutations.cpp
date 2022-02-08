40de592c874b679dc90a3aca260cd56635fed1f7
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-02-08 17:43:05 +0530

    Time: 4 ms (70.17%), Space: 7.5 MB (98.81%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    void helper(vector<int>& arr,vector<int>&visited ,vector<int>&currSet,vector<vector<int>>&res){
+        //Base Case 
+     if(currSet.size() == arr.size()){
+         
+         res.push_back(currSet) ;
+         return ;
+     }
+     
+    for(int i =0; i<arr.size(); i++){
+        if(visited[i] == 0){
+             visited[i] = 1 ;
+            currSet.push_back(arr[i]) ;
+            helper(arr,visited,currSet,res);
+            visited[i] = 0 ;
+            currSet.pop_back() ;
+        }
+    }
+        
+    }
+    
+    
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>res ;
+        vector<int>visited(nums.size(),0); 
+        vector<int>currSet ;
+        helper(nums,visited,currSet,res) ;
+        return res ;
+    }
+};

c12260c27de69f1ad58c73500e431dc47f98a325
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-02-12 15:10:29 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (18.13%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -11,19 +11,19 @@ public:
         dp[0] = 0 ;
         
         for(i = 1; i< n;i++){
-            if(s[i] == '(')
-                dp[i] = 0 ;
-            else{
-                L = dp[i-1] ;
+            
+               
+               if(s[i] == ')'){
+               
                 
-                if(i-L-1 < 0 || s[i-L-1] == ')')
-                    dp[i] = 0 ;
-                else{
+                if(i-dp[i-1]-1 >= 0 && s[i-dp[i-1]-1] == '('){
+                   
+               
                     
                     dp[i] = 2 + dp[i-1] ;
                     
-                    if(i-L-2 >= 0) 
-                        dp[i] += dp[i-L-2] ;
+                    if(i-dp[i-1]-2 >= 0) 
+                        dp[i] += dp[i-dp[i-1]-2] ;
                 }
             }
             

23837cb10de178732a9781e717bcceaf7c81e4fc
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-03-06 00:48:44 +0530

    Time: 4 ms (57.10%), Space: 5.9 MB (26.88%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        
+        if(x==1 || x==0) return x ;
+        
+       long long  int lo = 0, hi = x/2  ;
+        
+       long long  int  mid ;
+        
+        while(lo<hi){
+            
+             mid = lo +(hi-lo+1)/2 ;
+            
+            if(mid*mid == x) return mid ;
+            
+            if(mid*mid <= x)
+             lo = mid ;
+            
+            else 
+                hi = mid-1;
+        }
+        
+      return lo ;
+        
+       
+    }
+};
+
+
+
+// int Solution::sqrt(int A) {
+//     long long lo = 1, hi = A/2,mid ;
+    
+//     if(A==1) return 1;
+//     if(A==0) return 0;
+//     if(A==2) return 1;
+//     if(A==3) return 1;
+//     if(A == 4) return 2;
+//     int ans = 0;
+//     while(lo<=hi){
+//         mid = (hi + lo) / 2;
+        
+//         if(mid*mid == A)
+//             return mid;
+            
+//         if(mid*mid <= A) {
+//             lo = mid + 1;
+//             ans = mid;
+//         } else {
+//             hi = mid - 1;
+//         }
+ 
+//     } 
+//     return ans;
+
+// }

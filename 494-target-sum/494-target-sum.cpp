61429017fe8989fd909df1d4f141a4e70014d7b9
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-03-09 15:47:35 +0530

    Time: 238 ms (35.74%), Space: 13.8 MB (35.18%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -6,20 +6,22 @@ public:
         
       //  if(target>1000) return 0 ;
         
-        vector<vector<int>>dp(n+1,vector<int>(2001,0)) ;
+        vector<vector<int>>dp(2,vector<int>(2001,0)) ;
         
-        dp[n][0 + 1000] = 1 ;
+        dp[n%2][0 + 1000] = 1 ;
         
         for(i = n-1;i>=0;i--){
             for(j = -1000;j<= 1000; j++){
                 
+                dp[i%2][j+1000] = 0 ;
+                
                 //Two choices : add +a
                 if(j-nums[i] >= -1000)
-                    dp[i][j+1000] += dp[i+1][j-nums[i]+1000] ;
+                    dp[i%2][j+1000] += dp[(i+1)%2][j-nums[i]+1000] ;
                 // add -a 
                 
                 if(j+nums[i] <= 1000)
-                    dp[i][j+1000] += dp[i+1][j+nums[i]+1000]  ;
+                    dp[i%2][j+1000] += dp[(i+1)%2][j+nums[i]+1000]  ;
              }
             
         }

2fb800196d65c7f285e8549bfdfef7139b8e9a24
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-03-12 22:01:17 +0530

    Time: 6 ms (23.95%), Space: 9.7 MB (80.83%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -13,17 +13,23 @@ class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
         
+        //Base Case - If we reach NUll 
         if(root == NULL ) return NULL ;
-        
+        //if only one child is there 
         if(root->left == NULL && root->right == NULL) return root ;
         
+        //Connect left side to the right side 
         TreeNode* leftChild = root->left ;
         
         root->left = root->right ;
         
+        //Connect right to the left side 
         root->right = leftChild ;
         
+        //Call Recusively for left SUbtree 
         invertTree(root->left) ;
+        
+        //Call recursively for the RIght Subtree 
         
         invertTree(root->right) ;
         

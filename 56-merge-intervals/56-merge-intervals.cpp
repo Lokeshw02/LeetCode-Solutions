91912e1debda8fc0c6c6ceb67d3c020a43c34520
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-03-16 00:44:17 +0530

    Time: 57 ms (45.48%), Space: 18.8 MB (94.73%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -8,18 +8,18 @@ public:
         
         res.push_back(intervals[0]) ;
         
+       int j = 0 ;
         
-        int j = 0; 
-        
-        for(int i = 1; i< n; i++){
-            if(res[j][1] >= intervals[i][0])
-                res[j][1] = max(res[j][1],intervals[i][1]) ;
-            else{
-                j++ ;
-                res.push_back(intervals[i]) ;
-            }
-        }
-            
+       for(int i = 1; i< n;i++){
+           
+           if(res[j][1]>=intervals[i][0])
+               res[j][1] = max(res[j][1],intervals[i][1]) ;
+           else{
+               j++ ;
+               res.push_back(intervals[i]) ;
+           }
+           
+       }
             
             
             

22943175bb267d5eb2a2455335543efd6e8a8ac5
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-03-15 23:25:37 +0530

    Time: 31 ms (24.46%), Space: 13.6 MB (9.78%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
     string rankTeams(vector<string>& votes) {
-        //n is no of teams
-        //m is no of votes
-        int n = votes[0].size();
-        int m = votes.size();
+       
+        int n = votes.size() ; //No of Votes 
+        int m = votes[0].size() ; //No of teams 
         
-       vector<vector<int>>rank(26,vector<int>(n,0)) ;
+        vector<vector<int>>rank(26,vector<int>(m,0)) ;
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                rank[votes[i][j]-'A'][j]++ ;
+       for(int i = 0; i<n ;i++){
+            for(int j =0; j<m ;j++){
+                rank[votes[i][j] - 'A'][j] ++ ;
+                
             }
-    }
+        }
         
-    string res = votes[0];
-        sort(res.begin(),res.end(),[=](char&a,char&b){
-            for(int i=0;i<n;i++){
-                //if theere is a class level voting 
-                if(rank[a-'A'][i] == rank[b-'A'][i])
-                    continue;
-                //
-                else return rank[a-'A'][i]>rank[b-'A'][i]; 
-                    
+        string res = votes[0] ;
+        
+        sort(res.begin(),res.end(),[=](char &a, char &b){
+            
+            for(int i = 0 ; i < m ; i++){
+                
+                if(rank[a - 'A'][i] == rank[b - 'A'][i])
+                    continue ;
+                else 
+                    return rank[a - 'A'][i] > rank[b - 'A'][i] ;
+                
+                
             }
             return a<b ;
             
-        });
-        return res;
+            
+            
+        }) ;
+        
+       return res ;
     }
     
 };

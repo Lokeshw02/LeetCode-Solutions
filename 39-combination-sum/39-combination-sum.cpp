a6f3c718e317d1d8da98f77bb481825ce4e016e4
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-03-21 15:52:49 +0530

    Time: 11 ms (59.56%), Space: 10.8 MB (88.68%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    void helper(vector<int>& candidates, int sum, int startIdx,vector<int>&currSet, vector<vector<int>>& res){
+    
+    
+    void helper(vector<int>&candidates, int target, int startIdx,vector<int>&currSet,vector<vector<int>>&res){
         
-        if(sum ==0){
+        if(target == 0){
             res.push_back(currSet) ;
             return ;
         }
         
-        if(startIdx>=candidates.size()) return ;
+        if(startIdx >= candidates.size())
+            return ;
         
-        if(candidates[startIdx]>sum) return ;
+        if( target < candidates[startIdx] )
+            return ;
+        
         
         //Include 
+        
         currSet.push_back(candidates[startIdx]) ;
-        helper(candidates,sum-candidates[startIdx],startIdx,currSet,res) ;
+        
+        helper(candidates, target-candidates[startIdx],startIdx,currSet,res) ;
+        
         currSet.pop_back() ;
-        
-        
         
         //Exclude 
         
-         helper(candidates,sum,startIdx+1,currSet,res) ;
+         helper(candidates, target,startIdx+1,currSet,res) ;
+        
+          
+        
     }
     
     
@@ -28,12 +37,15 @@ public:
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         
-        sort(candidates.begin(),candidates.end()) ;
-        vector<int> currSet ;
-        vector<vector<int>> res ;
-         helper(candidates,target,0,currSet,res) ;
         
-        return res ;
+         sort(candidates.begin(),candidates.end()) ;
+         vector<int>currSet ;
+         vector<vector<int>>res ;
+         
+         helper(candidates,target,0,currSet,res) ;
+         
+         return res ;
+        
         
         
     }

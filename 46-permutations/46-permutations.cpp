aac0a43c66c190b8b2b49c8f66ca2fe5109086c5
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-03-21 17:33:30 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (78.99%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     
-    void helper(vector<int>& arr,vector<int>&visited ,vector<int>&currSet,vector<vector<int>>&res){
-        //Base Case 
-     if(currSet.size() == arr.size()){
-         
-         res.push_back(currSet) ;
-         return ;
-     }
-     
-    for(int i =0; i<arr.size(); i++){
-        if(visited[i] == 0){
-             visited[i] = 1 ;
-            currSet.push_back(arr[i]) ;
-            helper(arr,visited,currSet,res);
-            visited[i] = 0 ;
-            currSet.pop_back() ;
+    void helper(vector<int>&arr, vector<int>&visited, vector<int>&currSet,vector<vector<int>>&res){
+        
+        if(currSet.size() == arr.size()){
+            res.push_back(currSet) ;
+            return ;
         }
-    }
+        
+        for( int i = 0; i< arr.size() ;i++){
+            
+            if(visited[i] == 0){
+                visited[i] = 1 ;
+                currSet.push_back(arr[i]) ;
+                helper(arr,visited,currSet,res) ;
+            
+                visited[i] = 0 ;
+                currSet.pop_back() ;
+            }
+            
+            
+            
+        }
         
     }
-    
-    
-    
+     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>res ;
-        vector<int>visited(nums.size(),0); 
         vector<int>currSet ;
+        vector<int>visited(nums.size(),0) ;
         helper(nums,visited,currSet,res) ;
+        
         return res ;
     }
 };

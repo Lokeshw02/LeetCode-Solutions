62d6df9f12f7dcabfe401cf3e4f74c55bba71053
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-03-21 17:49:53 +0530

    Time: 4 ms (93.51%), Space: 8.6 MB (83.58%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,36 +1,47 @@
 class Solution {
 public:
     
-     void helper(vector<int>& arr,vector<int>&visited ,vector<int>&currSet,vector<vector<int>>&res){
-        //Base Case 
-     if(currSet.size() == arr.size()){
+     void helper(vector<int>&arr, vector<int>&visited, vector<int>&currSet, vector<vector<int>>&res){
+         if(currSet.size() == arr.size()){
+             res.push_back(currSet) ;
+             return ;
+         }
          
-         res.push_back(currSet) ;
-         return ;
+         for(int i = 0 ;i<arr.size();i++){
+             
+             if(visited[i] == 0){
+                 
+                 visited[i] = 1 ;
+                 currSet.push_back(arr[i]) ;
+                 helper(arr,visited,currSet,res) ;
+                 visited[i] = 0 ;
+                 currSet.pop_back() ;
+                 
+//                    while(i<arr.size()-1 && arr[i] == arr[i+1]) i++ ;
+                 
+                    int j ;
+                    
+                 for(j =i ; j<arr.size()-1 && arr[j] == arr[j+1];j++) ;
+                 i = j ;
+                 
+             }
+             
+          
+              
+             
+             
+         }
+             
+         
+         
+         
+         
      }
-     
-    for(int i =0; i<arr.size(); i++){
-        if(visited[i] == 0){
-             visited[i] = 1 ;
-            currSet.push_back(arr[i]) ;
-            helper(arr,visited,currSet,res);
-            visited[i] = 0 ;
-            currSet.pop_back() ;
-            
-            while(i<arr.size()-1 && arr[i] == arr[i+1]) i++ ;
-            
-        }
-    }
-        
-    }
-    
-    
-    
     
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         
         sort(nums.begin(),nums.end());
-         vector<vector<int>> res;
+        vector<vector<int>> res;
         vector<int>currSet;
         vector<int> visited(nums.size(),0);
         helper(nums,visited,currSet,res);

9eb79a8502bd84134fb94ef976eea42f06247930
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-03-23 23:59:32 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (72.64%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
          
-        int i = m-1;
-        int j = n-1 ;
-        
-        int k = nums1.size() -1 ;
+         int i= m-1;
+        int j = n-1;
+        int k = m+n-1 ;
         
         while(i>=0 && j>= 0){
             

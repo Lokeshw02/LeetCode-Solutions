92671f75516ffb591dafb98184a97a7dfdbc08d5
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-03-23 19:51:39 +0530

    Time: 231 ms (6.65%), Space: 67.7 MB (91.21%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
-    
-     int maxsum ;
-   
-    int helper(vector<int>& nums, vector<int>&dp, int n)
-	{
-		if (dp[n]!=-1)
-			return dp[n];
-		if (n == 0)
-			dp[n] = nums[0];
-		else
-		{
-			int d1 = helper(nums, dp, n - 1) + nums[n];
-			if (d1 > nums[n])
-			{
-				dp[n] = d1;
-			}
-			else
-			{
-				dp[n] = nums[n];
-			}
-		}
-		
-        if (dp[n] > maxsum)
-			maxsum = dp[n];
-        
-		return dp[n];
-	}
-
-    
-       
     int maxSubArray(vector<int>& nums) {
-        maxsum = INT_MIN;
-		vector<int>dp(nums.size(),-1) ;
         
-		
-		helper(nums, dp, nums.size() - 1);
-		return maxsum;
+        int n = nums.size() ;
+        
+    int prev = nums[0] ;
+    int curr ;
+    
+    int res = prev ;
+    
+    for(int i =1;i <n ;i++){
+        curr = max(nums[i], prev + nums[i]) ;
+        res = max(res, curr) ;
+        
+        prev = curr ;
+    }
+    
+    
+    return res ;
     }
 };

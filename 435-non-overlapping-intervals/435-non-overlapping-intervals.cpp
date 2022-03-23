98266ec96c79dc3879ec43616cea1b2f3fa5ca0b
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-03-23 12:00:25 +0530

    Time: 873 ms (14.59%), Space: 89.7 MB (83.04%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -4,7 +4,9 @@ public:
         
         int count = 0 ;
         
-        sort(intervals.begin(),intervals.end(),[=](vector<int>& t1, vector<int>&t2){
+        int n = intervals.size() ;
+        
+        sort(intervals.begin(),intervals.end(),[=](vector<int>&t1,vector<int>&t2){
             
             return t1[1]<t2[1] ;
             
@@ -14,16 +16,12 @@ public:
         
         int prev = intervals[0][1] ;
         
-        for(int i = 1; i<intervals.size(); i++){
+        for(int i = 1; i<n ;i++){
             
-            if(intervals[i][0] >= prev){
-                
+            if(intervals[i][0] >= prev)
                 prev = intervals[i][1] ;
-            }
             else 
-                count ++ ;
-                
-            
+                count++;    
             
         }
         

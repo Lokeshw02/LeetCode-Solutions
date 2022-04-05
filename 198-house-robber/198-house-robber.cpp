f485a8144385f456d9a3a7c4e5a101f20229ec90
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-04-05 12:29:59 +0530

    Time: 7 ms (11.07%), Space: 7.9 MB (24.98%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
     
-    int robHelper(vector<int>&nums,int startIdx,vector<int>&dp){
+    int robhelper(vector<int>&nums,vector<int>&dp,int startIdx){
+        
+        
+        
+        
+        if(startIdx >= nums.size())
+            return 0 ;
+        
+        
+        if(dp[startIdx] != -1) return dp[startIdx] ;
+        
+        
+        //Thief starts from the startIdx house 
+        
+       int  m1 = nums[startIdx]+ robhelper(nums,dp,startIdx+2) ;
+            
+        // If it does not include startIdx
+        
+       int  m2 = robhelper(nums,dp,startIdx+1);
+        
+            
+        dp[startIdx] = max(m1,m2) ;
+        
+        return dp[startIdx] ;
+        
+        
+        
+    }
       
-       int n = nums.size();
-      
-       if(startIdx>=n) return 0;
-     
-       if(dp[startIdx] != -1) return dp[startIdx];
-      
-       int  money1 = nums[startIdx]+robHelper(nums,startIdx+2,dp);
-       
-       int money2 = robHelper(nums,startIdx+1,dp);
-      
-       dp[startIdx] = max(money1,money2);
-       
-        return dp[startIdx];
-   }
     
     
     int rob(vector<int>& nums) {
         
         vector<int>dp(nums.size(),-1) ;
         
-        return robHelper(nums,0,dp) ;
-        
+        return robhelper(nums,dp,0) ;
         
         
     }

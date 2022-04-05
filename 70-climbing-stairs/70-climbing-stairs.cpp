db4b5f39722c495eabb7c9dbe29c3ec9803d2dc4
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-04-05 12:45:45 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (29.78%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -4,17 +4,19 @@ public:
     
     int climbHelper(int start,int n,vector<int>&dp){
         
-        if(start == n) return 1;
+        if(start == n) return 1 ;
         
-        if(start>n) return 0 ;
+        if(start > n) return 0 ;
         
-        if(dp[start] != -1) return dp[start]  ;
+        if(dp[start] != -1) return dp[start] ;
         
-        int res =  climbHelper(start+1,n,dp) ;
+        int res = climbHelper(start+1,n,dp) ;
         
         res += climbHelper(start+2,n,dp) ;
         
+        
         dp[start] = res ;
+        
         
         return dp[start] ;
         

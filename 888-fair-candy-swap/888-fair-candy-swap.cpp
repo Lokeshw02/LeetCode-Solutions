07d2d1641d0c7a89df58cf06163ee8cc73622fc4
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-06-20 20:01:22 +0530

    Time: 126 ms (67.76%), Space: 39.1 MB (92.51%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.cpp
+++ 888-fair-candy-swap/888-fair-candy-swap.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+    
+    
+    bool BS(vector<int>& bobSizes, int s){
+        
+        
+        
+        
+        int lo = 0 ;
+        int hi = bobSizes.size()-1 ;
+        
+        
+        while(lo<hi){
+            
+            int mid = lo + (hi-lo)/2 ;
+            
+            
+        if(bobSizes[mid] >= s )
+            hi = mid ;
+            
+        else
+            lo = mid +1 ;
+        }
+        
+        
+        
+        
+      return bobSizes[lo] == s ; 
+         
+        
+    }
+    
+    
+    
+ 
     vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
         
         int alicSum = 0, bobSum = 0 ;
@@ -10,34 +44,52 @@ public:
         
         for(int x : bobSizes) bobSum += x ;
         
-        unordered_map<int,int> m ;
+//         unordered_map<int,int> m ;
         
-        for(int y : bobSizes) m[y] = 1 ;
-        
-        
+//         for(int y : bobSizes) m[y] = 1 ;
         
         
+        sort(bobSizes.begin(),bobSizes.end()) ;
+        
+     
         
         for(int i =0 ;i < aliceSizes.size();i++){
             
-            //If present 
-            if(m.find(aliceSizes[i]+ (bobSum-alicSum)/2 ) != m.end()){
+//             //If present 
+//             if(m.find(aliceSizes[i]+ (bobSum-alicSum)/2 ) != m.end()){
                
-                
-             res.push_back(aliceSizes[i]) ;
-             res.push_back(aliceSizes[i]+ (bobSum-alicSum)/2) ;
+            
+//              res.push_back(aliceSizes[i]) ;
+//              res.push_back(aliceSizes[i]+ (bobSum-alicSum)/2) ;
             
                 
-             break ;
+//              break ;
                 
+//             }
+            
+            if(BS(bobSizes,aliceSizes[i]+ (bobSum-alicSum)/2 )){
+                
+                res.push_back(aliceSizes[i]) ;
+                res.push_back(aliceSizes[i]+ (bobSum-alicSum)/2) ;
+                break ;
+               
             }
+            
+            else{
+                
+                continue ;
+            }
+                
+            
+            
                 
     
                 
             }
-                       
-            
-    
+        
+        
+        
+                      
     
         
         return res ; 

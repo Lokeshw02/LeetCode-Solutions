f1f3cec2db7eedb3fe8c91b735efa89016fd593b
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-06-25 19:28:16 +0530

    Time: 30 ms (38.77%), Space: 18.6 MB (49.49%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.cpp
+++ 275-h-index-ii/275-h-index-ii.cpp
@@ -2,19 +2,32 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         
-        int lo=0, hi = citations.size()-1, mid;
-        int len = citations.size();
+//         int lo=0, hi = citations.size()-1, mid;
+//         int len = citations.size();
             
-        while(lo<=hi)
-        {
-            mid= lo + (hi-lo)/2 ;
+//         while(lo<hi)
+//         {
+//             mid= lo + (hi-lo)/2 ;
             
-            if(citations[mid]== (len-mid)) return citations[mid];
-            else if(citations[mid] > (len-mid)) hi = mid - 1;
-            else lo = mid + 1;
-        }
+//             if(citations[mid]>= (len-mid))
+//                 hi = mid ;
+            
+//             else
+//                 lo = mid + 1;
+//         }
         
-        return len - (hi+1);
+//         return len - lo;
+        
+        
+        
+        int left=0, len = citations.size(), right= len-1,  mid;
+        while(left<=right)
+        {
+            mid=left+ (right-left)/2;
+            if(citations[mid] >= (len-mid)) right = mid - 1;
+            else left = mid + 1;
+        }
+        return len - left;
         
     }
 };

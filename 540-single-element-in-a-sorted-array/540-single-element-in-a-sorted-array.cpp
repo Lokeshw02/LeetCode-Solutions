1ed86a6c98f32824c3dc4cbd7499562231c0c6dc
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-07-01 15:54:47 +0530

    Time: 38 ms (57.28%), Space: 22.3 MB (91.14%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+       
         
-        int XOR = 0 ;
+        if(nums.size() == 1) 
+            return nums[0] ;
         
-        for(int i : nums){
-            XOR = XOR^i ;
+        int lo = 0 ;
+        int hi = nums.size() -1 ;
+        
+        int mid ;
+        
+        while(lo<=hi){
             
+            mid = lo + (hi-lo)/2 ;
+            
+            //If mid is Odd -> If sequence is good till here, the element at Index mid -1 will be same as that on mid  OR  If mid is Even and sequence till here is good, then the element mist be equal to element at Index mid +1 ; 
+            
+            
+            
+            
+            if (mid % 2 == 1 && nums[mid] == nums[mid-1] || mid % 2 == 0 && nums[mid] == nums[mid+1] )
+                lo = mid +  1 ;
+            else 
+                hi = mid -1 ;
+ 
+                
         }
-        return XOR ;
+        
+        return nums[lo] ;
     }
 };

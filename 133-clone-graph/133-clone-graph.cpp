5c09c3e0d2c6f88f4c33201f89c21618d5c29e1c
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-07-21 16:15:33 +0530

    Time: 20 ms (5.13%), Space: 8.9 MB (32.89%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,52 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+    Node* dfs(Node* n, unordered_map<Node*,Node*> &m){
+        Node *newN = new Node(n->val) ;
+        m[n] = newN ;
+        
+        //Go through the unvisited Neighbours 
+        
+        for(auto &nbr : n->neighbors){
+            if(m.find(nbr) == m.end())
+                newN->neighbors.push_back(dfs(nbr,m)) ;
+            else 
+                newN->neighbors.push_back(m[nbr]);
+        }
+        return newN ;
+    }
+    
+  
+    
+    
+    Node* cloneGraph(Node* node) {
+        
+        if(!node)
+            return NULL ;
+        
+        unordered_map<Node*,Node*> m ;
+        
+        return dfs(node,m) ;
+    }
+};

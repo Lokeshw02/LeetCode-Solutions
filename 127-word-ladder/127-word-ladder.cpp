83f36097842e8c38b91cef47fb23933fb060e0aa
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-07-31 16:26:44 +0530

    Time: 213 ms (63.18%), Space: 15.7 MB (46.52%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        // Construct the dictionary
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        
+        // BFS
+        // List i.e. Queue
+        // Visited
+        unordered_set<string> visited;
+        queue<pair<string, int>> q;
+        
+        // Insert the first word in the queue
+        q.push({beginWord, 0});
+        visited.insert(beginWord);
+        
+        // Start BFS
+        while(!q.empty()) {
+            string curr_word = q.front().first;
+            int dist = q.front().second;
+            q.pop();
+            
+            if(curr_word == endWord) {
+                return dist+1;
+            }
+            
+            // Go through the neighbours of curr_word
+            // Generate neighbours
+            string temp_word = curr_word;
+            for(int i = 0; i < curr_word.size(); i++) {
+                for(int j = 0; j < 26; j++) {
+                    temp_word[i] = (char)(j+'a');
+                    
+                    // If exists in the dictionary and not visited yet
+                    // Add it to the unexplored list
+                    if(visited.find(temp_word) == visited.end() && dict.find(temp_word) != dict.end()) {
+                        visited.insert(temp_word);
+                        q.push({temp_word, dist+1});
+                    }
+                    
+                    temp_word[i] = curr_word[i];
+                }
+            }
+            
+        }
+        
+        return 0;
+        
+        
+    }
+};

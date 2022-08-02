a36eb4972ce4fa1692e5b3b55d2dbe5b54a7fbb7
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-08-02 21:02:09 +0530

    Time: 21 ms (82.62%), Space: 24.3 MB (18.96%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,59 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>>dirs = {{1,0},{0,1},{-1,0},{0,-1}} ;
     
-    bool isValidX(int x, int m){
-        return (x>=0 && x<m) ;
-    }
     
-     bool isValidY(int y, int n){
-        return (y>=0 && y<n) ;
-    }
+    vector<vector<int>> dirs = {{0,1},{1,0},{0,-1},{-1,0}} ;
     
-    bool isValidCell(int x, int y, int m, int n){
-     
-        return isValidY(y,n) && isValidX(x,m) ;
-    
-    }
-    
-    bool isValidNode(int x, int y, vector<vector<int>>& grid){
-        return isValidCell(x,y,grid.size(),grid[0].size()) && grid[x][y] ;
-    }
-    
-    void dfs(int i, int j, vector<vector<bool>>& visited, vector<vector<int>>& grid, int &count ){
-      
-    visited[i][j] = true ;
-    count ++ ;
-    //Now visit the neighbors 
-    
-    for(int t = 0 ; t < dirs.size() ; t++){
+    void dfs(int i, int j, vector<vector<bool>>& visited, vector<vector<int>>&grid, int &count ){
+        visited[i][j] = true ;
+        count ++ ;
+        //Now visit the Neighbours 
         
-        int x = i + dirs[t][0] ;
-        int y = j + dirs[t][1] ;
-        
-        //x and y are the neighbors of i,j 
-        //Chek if it is a node 
-        
-        if(isValidNode(x,y,grid) && !visited[x][y]) 
-            dfs(x,y,visited,grid,count) ;
-        
-
+        for(int t = 0; t<dirs.size(); t++){
+            int x = i + dirs[t][0] ;
+            int y = j + dirs[t][1] ;
+            
+            //x and y are 4 neighbours of i,j 
+            
+            if(x >=0 && x < grid.size() && y >= 0 && y < grid[0].size() && grid[x][y] == 1 && !visited[x][y])
+                dfs(x,y,visited,grid,count) ;
+        }
     }
-        
     
-    }
+    
+    
+    
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int i,j, count = 0 , res =  0 ;
-        vector<vector<bool>>visited(grid.size(), vector<bool>(grid[0].size(), false)) ;
+        int i, j, count = 0, res = 0 ;
+        vector<vector<bool>>visited(grid.size(),vector<bool>(grid[0].size(), false )) ;
         
-        for(int i = 0 ; i<grid.size(); i++){
-            for(int j = 0; j<grid[0].size(); j++){
+        for(int i = 0; i < grid.size() ; i++){
+            for(int j = 0; j < grid[0].size() ; j++){
                 
-                if(grid[i][j] == 1 && !visited[i][j]){
+                if(grid[i][j] == 1 && !visited[i][j] ){
                     count = 0 ;
-                    dfs(i,j,visited,grid,count) ; 
-                    res = max(res, count) ;
+                    dfs(i,j,visited,grid,count) ;
+                    res = max(res,count) ;
                 }
             }
         }

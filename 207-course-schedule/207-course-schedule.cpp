6f7b02c9069cc51021f08103fae54f6f747cd649
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-08-04 01:18:34 +0530

    Time: 39 ms (41.91%), Space: 13.9 MB (60.02%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,58 +1,57 @@
 class Solution {
 public:
     
-    bool isCycle(int n, vector<bool>& visited, vector<bool>& path, vector<vector<int>>& adjList){
+    
+    bool isCycle(int n, vector<bool>& visited, vector<bool>&path, vector<vector<int>>&adjList){
         
         visited[n] = true ;
         path[n] = true ;
         
         bool t = false ;
         
-        //Go through all the neighbours of n 
-        
         for(auto &nbr : adjList[n]){
-            //Cycle
-            if(path[nbr])
+            // If cycyle Exists 
+            if(path[nbr]) 
                 return true ;
-            //Otherwise traverse this edge if not visiste 
-            if(!visited[nbr])
-                t = t || isCycle(nbr, visited, path, adjList) ;
+            //Otgherwise travverse this edge if ot viviste d
+            
+            if(!visited[nbr]){
+                t = t || isCycle(nbr,visited,path,adjList) ;
+            }
             
             if(t)
                 return true ;
+            
+            
         }
-        
         path[n] = false ;
         
         return t ;
     }
     
-    
-    
-    
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-    //Adjecency List 
+    
         vector<vector<int>>adjList(numCourses) ;
         
-        for(auto &prereq : prerequisites){
-            //(a,b) is the prereq 
-            //Add an edge from b to a 
-            adjList[prereq[1]].push_back(prereq[0]) ;
+        for(auto & pre : prerequisites ){
+            adjList[pre[1]].push_back(pre[0]) ;
             
         }
         
-        vector<bool> visited(numCourses, false), path(numCourses,false) ;
+        vector<bool> visited(numCourses,false) , path(numCourses, false); 
         
         bool t = false ;
         
-        for(int i =0 ; i < numCourses ; i++){
+        for( int i = 0 ; i < numCourses ; i++){
             if(!visited[i])
-                t = t || isCycle(i, visited, path, adjList) ;
+                t = t || isCycle(i,visited, path,adjList) ;
             
             if(t)
                 return false ;
         }
         
         return !t ;
+        
+        
     }
 };

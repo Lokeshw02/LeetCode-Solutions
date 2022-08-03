2419820b92a96731547cdf82ef54ad6e548de605
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-08-03 17:29:30 +0530

    Time: 29 ms (23.63%), Space: 10.4 MB (28.83%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,60 +1,64 @@
 class Solution {
 public:
     
-   vector<vector<int>> dir = {{0,-1},{0,1},{-1,0},{1,0}} ;
+    vector<vector<int>>dirs = {{0,1},{1,0},{-1,0},{0,-1}} ;
     
-    void dfs(int i, int j,vector<vector<bool>> &visited,vector<vector<char>>& board){
+    void dfs(int i, int j, vector<vector<bool>>&visited, vector<vector<char>>& board){
         
-        if(i<0 || i>= board.size() || j<0 || j>=board[0].size())   return ;
-          
+        if(i<0 || i>= board.size() || j<0 || j>= board[0].size()) 
+            return ;
         if(board[i][j] == 'X' || visited[i][j]) return ;
         
         visited[i][j] = true ;
         
-         for(int t = 0 ; t<dir.size(); t++){
-             int x = i + dir[t][0] ;
-             int y = j + dir[t][1] ;
-             
-             dfs(x,y,visited,board) ;
-             
-         }
-        
+        for(int t= 0; t < dirs.size(); t++){
+            int x = i + dirs[t][0] ;
+            int y = j + dirs[t][1] ;
+            
+            dfs(x,y,visited,board) ;
+        }
     }
     
+    
     void solve(vector<vector<char>>& board) {
-     if(board.size() == 0 ) return ;
-     
-  
-    vector<vector<bool>>visited (board.size(), vector<bool>(board[0].size(),false)) ;
+        vector<vector<bool>>visited(board.size(),vector<bool>(board[0].size(),false)) ;
         
-        //Do a dfs from boundary 
-        //Top and Bottom Boundary 
         
-        for(int i = 0; i <board[0].size(); i++){
-            //Top Boundary 
-            dfs(0,i,visited,board) ;
-            //Bottom Boundary 
-            dfs(board.size()-1,i,visited,board) ;
-        }
-        
-        //Left and Right Boundaries 
-        
-        for(int j = 0; j < board.size();j++){
-            //Left 
-            dfs(j,0,visited,board) ; 
-            //Right 
-            dfs(j,board[0].size()-1,visited,board) ;
-            
+      if(board.size() == 0 ) return ; 
+                                   
+                                   //Do a DFS from boundary 
+       
+         for(int i = 0; i < board.size(); i++){
+             //Left
+             dfs(i,0,visited,board) ;
+             //Rigth 
+             dfs(i,board[0].size()-1,visited, board) ;
+             
+         }
+         //Top and Bottom 
+                                  
+         for(int j = 0; j < board[0].size(); j++){
+             //top 
+             dfs(0,j,visited,board) ;
+             //Bottom 
+             dfs(board.size()-1,j,visited, board) ;
+             
+         } 
+                                   
+         //Flip the Unvisited 0 
+        for(int i = 0; i< board.size(); i++){
+            for(int j = 0 ;j< board[0].size(); j++){
+                
+                if(board[i][j] == 'O' && !visited[i][j])
+                    board[i][j] = 'X' ;
             }
-            
-            //Flip the Unvisited O 
-            
-            for(int i = 0 ; i< board.size(); i++){
-                for(int j = 0 ; j< board[0].size(); j++){
-                    if( (board[i][j] == 'O') && !visited[i][j] )
-                        board[i][j] = 'X' ;
-                }
-            }
-        
+        }                           
+
+ 
+                                   
+                                 
+      
+                                   
+                            
     }
 };

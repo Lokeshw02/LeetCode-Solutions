3f4f0afef79688e4957580e9eb22a886f0ac15ae
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-08-04 01:47:18 +0530

    Time: 269 ms (47.34%), Space: 47.3 MB (67.41%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,47 +1,48 @@
 class Solution {
 public:
-    int hasCycle(vector<vector<int>>&graph, vector<int>& visited, vector<bool>& path, int n ){
+    
+    int hasCycle(vector<vector<int>>& graph, vector<int>& visited, vector<bool>& path, int n){
         path[n] = true ;
-        //Safe is 1 , unsafe is 2 
-        //Not visited is 0 
-        int safe = 1, t ;
+        //Safe 1 // Unsafe 2 // Unvisited 0 
+        
+        int safe = 1 , t ;
+        
         for(auto &nbr : graph[n]){
-            if(path[nbr])
-                safe = 2;
-            else if(!visited[nbr]){
-                t  = hasCycle(graph, visited,path, nbr) ;
-                if(t ==2 )
+            
+            if(path[nbr]) 
+                safe = 2 ;
+            else if (!visited[nbr]){
+                t = hasCycle(graph, visited, path, nbr) ;
+                if(t ==2) 
                     safe = t ;
-                
             }
             else if(visited[nbr] == 2 )
-                safe = visited[nbr] ;
+                safe  = visited[nbr] ;
         }
-        path[n] = false ;
+        path[n]  = false ;
         visited[n] = safe ;
         
         return safe ;
     }
     
+    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int numNodes = graph.size() ;
         vector<int> visited(numNodes,0) ;
-        vector<bool>path(numNodes,0) ;
+        vector<bool> path(numNodes,0) ;
         
-        vector<int> safeList ;
+        vector<int>safeList  ; 
         
-        for(int i = 0; i< numNodes ; i++){
-            if(!visited[i]){
-                int t = hasCycle(graph, visited, path , i) ;
-            }
+        for( int i =0 ; i < numNodes ; i++){
+            if(!visited[i])
+                int t = hasCycle(graph,visited,path,i) ;
         }
         
-        for(int i = 0; i< numNodes ; i++){
-        if(visited[i] == 1)
-            safeList.push_back(i) ;
+        for( int i =0 ; i < numNodes ; i++){
+            if(visited[i] ==1 )
+                safeList.push_back(i) ;
         }
+        
         return safeList ;
-        
-        
     }
 };

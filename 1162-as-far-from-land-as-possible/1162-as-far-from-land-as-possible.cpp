4c332170abb83c7ccd0ef5aa53e72c287b8b0cc7
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-08-09 02:53:32 +0530

    Time: 407 ms (6.42%), Space: 49.2 MB (6.98%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,58 +1,56 @@
 class Solution {
 public:
     
-     vector<vector<int>> dirs = {{0,1},{1,0},{0,-1},{-1,0}} ;
+      vector<vector<int>> dirs = {{0,1},{1,0},{0,-1},{-1,0}} ;
     
-    
-    int maxDistance(vector<vector<int>>& mat) {
+    int maxDistance(vector<vector<int>>& grid) {
         
-         int  n = mat.size() , m = mat[0].size(), dist = -1 ;
-        vector<vector<bool>>visited(m,vector<bool>(n,false)) ;
-      
+         int  n = grid.size() , m = grid[0].size() ;
+        vector<vector<bool>>visited(n,vector<bool>(m,false)) ;
+      //  vector<vector<int>>dist(n,vector<int>(m,0)) ;
         
         //q[0] -> x, q[1] ->y, q[2]-> level 
         
+        int res = -1;
         
-        queue<vector<int>> q ;
+        queue<vector<int>>q ;
         
-        //Push all the 1s 
-        for(int i =0; i< m ; i++){
-            for(int j =0; j<n ; j++){
+        //Push all the ones 
+        for(int i =0; i< n ; i++){
+            for(int j =0; j<m ; j++){
 
-                if(mat[i][j] == 1){
+                if(grid[i][j] == 1){
                     q.push({i,j,0}) ;
                     visited[i][j] = true ;
-                    
+                    //dist[i][j] = 0 ;
                 }
                  
             }
         }
-        
         //Read to Start BFS 
         while(!q.empty()){
             vector<int> t = q.front();
             q.pop() ;
+            
             //Push its nbrs 
             
             for(auto &drs : dirs){
                 int x = t[0] + drs[0] ;
                 int y = t[1] + drs[1] ;
                 
-                if(x<0 || x>=m || y<0 || y>= n) continue ;
-                   
-                
+                if(x<0 || x>=n || y<0 || y>= m)
+                    continue ;
                 
                 if(visited[x][y]) continue ;
                 
                 q.push({x,y,t[2]+1}) ;
                 visited[x][y] = true ;
-                dist = max(dist,t[2] + 1 ) ;
+                res = max(res,t[2] + 1)  ;
                 
             }
         
         }
             
-        return dist ;
-        
+       return res ; 
     }
 };

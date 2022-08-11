f15719d418e57cad09d2b5a02fa58a1bf32a7960
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-08-11 14:55:28 +0530

    Time: 170 ms (69.24%), Space: 44.6 MB (19.25%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,48 +1,38 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        //Explicit graph Creation 
-        //Adjecency List 
-        vector<vector<pair<int,int>>>graph(n+1) ;
-        vector<bool> visited(n+1, false) ;
+        vector<vector<pair<int,int>> > graph(n+1) ;  
+        vector<int> visited(n+1,false) ;
         
-        int res =-1, count = 0 ;
-        
-        for(auto &time : times){
+        for(auto time : times){
             graph[time[0]].push_back({time[1],time[2]}) ;
-            
         }
-        //Weighted BFS 
-        //Minimum Priority Quieue 
-        //Evevry element is a pair <-- first as the weight , second is the node number 
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>> > pq ;
         
-        //Initialise 
+        int res = -1 , count = 0 ;
+        
+        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>> > pq ;
+        //First as the Weight // Second as the Node Number 
+        
         pq.push({0,k}) ;
         
         while(!pq.empty()){
-            pair<int,int>t = pq.top() ;
+            pair<int,int> t = pq.top() ;
             pq.pop() ;
             
-            //Ignore already visited Nodes 
-            
+            //Ignore already vissited Nodes 
             if(visited[t.second]) continue ;
-            
-            //shortest path to this node has been computed 
             res = max(res, t.first) ;
-            //Mark it as Visited 
             visited[t.second] = true ;
             count++ ;
+            //Push the unvisited neighbours 
             
-            //Push its unvisited neighbours 
-            for(auto &nbr : graph[t.second]){
+            for(auto &nbr : graph[t.second] ){
                 if(!visited[nbr.first]){
-                    pq.push({t.first + nbr.second, nbr.first}) ;
+                    pq.push({t.first+nbr.second, nbr.first}) ;
                 }
             }
-            
         }
         
-        return count  == n ? res : -1 ;
+        return count == n ? res : -1 ;
     }
 };

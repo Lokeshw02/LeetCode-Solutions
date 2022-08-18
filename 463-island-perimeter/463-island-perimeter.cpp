046353b48146e2207e8f92d486ae53082f2ad8f3
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-08-18 16:34:48 +0530

    Time: 202 ms (35.43%), Space: 98 MB (17.96%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -4,36 +4,32 @@ public:
     vector<vector<int>> dirs = {{0,1},{1,0},{0,-1},{-1,0}} ;
     
     
-    int sideAdder(int i, int j, vector<vector<int>>& grid ){
+    int sideAdder(int i, int j, vector<vector<int>>& grid, int n, int m ){
         
         int sides = 0 ;
         
-        for(int t = 0; t < dirs.size() ; t++){
-            int x = i + dirs[t][0] ;
-            int y = j + dirs[t][1] ;
+        for(auto &dir : dirs){
+            int x = i + dir[0] ;
+            int y = j + dir[1] ;
             
-            if(x < grid.size()  && x >= 0 && y < grid[0].size() && y >= 0 && grid[x][y] == 0 || x == -1 || x == grid.size() || y == grid[0].size() || y == -1 ) 
+            if(x < grid.size()  && x >= 0 && y < grid[0].size() && y >= 0 && grid[x][y] == 0 || x == -1 || x == n || y == m || y == -1 ) 
                 sides++ ;
                 
         }
         
         return sides ;
-        
-        
-        
+      
     }
-    
-    
-    
-    
-    int islandPerimeter(vector<vector<int>>& grid) {
+ int islandPerimeter(vector<vector<int>>& grid) {
         
         int res = 0;
+        int n = grid.size() ;
+        int m = grid[0].size() ;
         
         for(int i=0; i< grid.size(); i++){
             for(int j = 0; j<grid[0].size() ; j++){
                 if(grid[i][j] == 1)
-                  res += sideAdder(i,j,grid) ;  
+                  res += sideAdder(i,j,grid,n,m) ;  
             }
         }
         

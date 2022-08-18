f0358d9ca04715f6a6965e388f57add98dfc95b8
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-08-18 13:13:12 +0530

    Time: 139 ms (6.37%), Space: 26.1 MB (5.06%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -3,49 +3,36 @@ public:
     
     vector<vector<int>> dirs = {{0,1},{1,0},{-1,0},{0,-1}} ;
     
-    
-    void dfs(int i, int j,vector<vector<char>>& grid, vector<vector<bool>>& visited){
+    void dfs(vector<vector<char>>& grid, vector<vector<bool>>&visited, int i, int j, int n, int m){
         
-        visited[i][j] = true ;
-        int n  = grid.size() ;
-        int m = grid[0].size() ;
+        visited[i][j]  = true ;
         
-        for(int t = 0 ; t< dirs.size(); t++){
-            int x = i + dirs[t][0] ;
-            int y = j + dirs[t][1] ;
+        // Go through neigbours 
+        
+        for(auto dir : dirs){
+            int x = i + dir[0] ;
+            int  y = j + dir[1] ;
             
-          if(x >= 0 && x < n && y < m && y >=0 && grid[x][y] == '1' && !visited[x][y])
-             dfs(x,y,grid,visited) ;
-            
-           
+            if(x >= 0 && y >= 0 && x < n && y < m && !visited[x][y] && grid[x][y] == '1' )
+                dfs(grid, visited, x, y, n, m) ;
         }
     }
     
-    
-    
     int numIslands(vector<vector<char>>& grid) {
-        
         int n = grid.size() ;
         int m = grid[0].size() ;
         
+        vector<vector<bool>>visited(n,vector<bool>(m,false)) ;
         int count = 0 ;
-      
-        vector<vector<bool>> visited(n, vector<bool>(m,false)) ;
         
-        for(int i = 0 ; i < n; i++){
-            for(int j = 0 ; j < m ; j++){
-                
-                if(!visited[i][j] && grid[i][j] == '1'){
-                   
-                    dfs(i,j,grid,visited) ;
-                     count++ ;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0; j< m ; j++){
+                if(grid[i][j] == '1' && !visited[i][j]){
+                    dfs(grid, visited, i, j, n, m) ;
+                    count++ ;
                 }
-                    
             }
         }
-        
-    
-       return count ;
-       
+        return count ;
     }
 };

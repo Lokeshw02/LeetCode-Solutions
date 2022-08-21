8ef903718b94391b3ff18fb8e3c43867726c3b5b
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-08-21 20:29:24 +0530

    Time: 26 ms (66.53%), Space: 24.9 MB (16.55%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,55 +1,48 @@
 class Solution {
 public:
     
+    vector<vector<int>> dirs = {{0,1}, {0,-1}, {1,0}, {-1,0} } ;
     
-    vector<vector<int>>dirs = {{1,0},{0,1},{-1,0},{0,-1}} ;
-    
-    void dfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, int &count, int i, int j, int &n, int &m){
-        visited[i][j] = true ;
+    void dfs(vector<vector<int>>& grid, vector<vector<bool>>&dp, int i, int j, int &count , int &n, int &m){
         
+        dp[i][j] = true ;
         count++ ;
         
-        //Go through the neighbours 
-        
-        for(auto &dir : dirs){
+        for(auto & dir : dirs){
             int x = i + dir[0] ;
-            int y = j + dir[1] ; 
+            int y = j + dir[1] ;
             
-            if(x >= 0 && y >=0 && x < n && y <m && grid[x][y] == 1 && !visited[x][y]){
-                
-                 
-                 dfs(grid,visited,count,x,y,n,m)  ; 
-                  
-                
-            }
-               
+            if(x < n && x >= 0 && y < m && y >= 0 && !dp[x][y] && grid[x][y]) 
+                dfs(grid,dp,x,y,count,n,m) ;
+            
         }
         
         
-        
+       
     }
     
     
+    
     int maxAreaOfIsland(vector<vector<int>>& grid) {
+        
         int n = grid.size() ;
         int m = grid[0].size() ;
-    
-        vector<vector<bool>>visited(n,vector<bool>(m,false)) ;
+        vector<vector<bool>>dp(n, vector<bool>(m,false)) ;
+        
         int count, res = 0 ;
         
-        for(int i = 0; i< n ; i++){
-            for(int j =0 ; j< m ;j++){
-                
-                if(!visited[i][j] && grid[i][j] == 1 ){
-                    
-                    count = 0 ;
-                    dfs(grid,visited,count,i,j,n,m) ;
-                    res = max(res,count) ;
-                    
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0; j< m ; j++){
+                if(!dp[i][j] && grid[i][j]){
+                     count = 0 ;
+                     dfs(grid,dp,i,j,count,n,m);
+                     res = max(res,count) ;
                 }
+                 
             }
         }
-       // return res == INT_MIN ? 0 : res ;
+    
+     
     return res ;
     }
 };

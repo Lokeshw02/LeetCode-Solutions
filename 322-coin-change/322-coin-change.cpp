ae70578e2aa084ebb1398ca8f6224dac5e137b59
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-08-24 18:52:04 +0530

    Time: 208 ms (23.72%), Space: 49.4 MB (14.65%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,22 +1,48 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-      
-        int n = coins.size(),i,j, temp ;
-        vector<int>dp(amount+1,INT_MAX) ;
         
-        dp[0] = 0 ;
+       int n = coins.size() ;
         
-        for(i = n-1; i>= 0; i--){
-            for(j=0;j<=amount;j++){
-                //Include 
-                if(j-coins[i] >= 0){
-                    if(dp[j-coins[i]] != INT_MAX)
-                        dp[j] = min(dp[j],1+dp[j-coins[i]]) ;
-                }
-            }
-        }
+       // if(n == 1){
+       //     if(coins[0] == amount ) return 1 ;
+       //     if(amount % coins[0] == 0 ) return amount/coins[0] ;
+       //     return -1 ;
+       // } 
+       vector<vector<int>> dp(n+1, vector<int>(amount +1) );
+      /////////  
+       for(int i = 0 ;i < amount +1  ; i++){
+          
+                dp[0][i] = INT_MAX -1  ;
+            
+       }
+         for(int j = 1 ;j < n +1  ; j++){
+          
+                dp[j][0] = 0  ;
+            
+       }
+         for(int j = 1 ;j < amount +1  ; j++){
+          
+                if(j%coins[0] == 0) dp[1][j] = j/coins[0] ;
+                else 
+                    dp[1][j] = INT_MAX -1 ;
+            
+       }
         
-        return dp[amount] == INT_MAX ? -1 : dp[amount] ;
+        
+        /////////////////
+        
+         for(int i = 2 ;i < n+1 ; i++){
+           for(int j = 1 ; j< amount+1 ; j++){
+              
+               if(coins[i-1] <= j)
+                   dp[i][j] =  min (1 + dp[i][j-coins[i-1]] , dp[i-1][j] )  ;
+               else 
+                   dp[i][j] = dp[i-1][j] ;
+           }
+       }
+        
+        return dp[n][amount] == (INT_MAX -1) ? -1 : dp[n][amount];
+        
     }
 };

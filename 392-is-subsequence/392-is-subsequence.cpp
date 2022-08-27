a52719b70454fa313728c499bf48327f0576676e
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-08-27 17:23:36 +0530

    Time: 8 ms (12.55%), Space: 7.9 MB (9.28%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+          
+        int n = s.size(), m =t.size() ; 
+        string text1 = s, text2 = t ;
+        
+        
+        
+        vector<vector<int>>dp(n+1,vector<int>(m+1)) ; 
+        
+        for(int i = 0 ; i < n; i++){
+            for(int j = 0; j < m ; j++){
+                
+                if(i == 0 || j == 0) dp[i][j] = 0 ;
+            }
+        }
+        
+        for(int i = 1 ; i < n+1; i++){
+            for(int j = 1; j < m+1 ; j++){
+                
+                if(text1[i-1] == text2[j-1]) 
+                    dp[i][j] = 1 + dp[i-1][j-1] ;
+                
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]) ;
+            }
+        }
+        
+        return dp[n][m] == s.size() ? true : false  ;
+        
+    }
+};

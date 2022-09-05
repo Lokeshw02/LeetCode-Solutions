71370aab1f2dac582d47dbcdf5beda161569459e
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-06 00:38:36 +0530

    Time: 7 ms (54.23%), Space: 10.8 MB (99.99%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,94 +1,62 @@
 class Solution {
 public:
-    
-    int searchPivot(vector<int>&nums){
+    int search(vector<int>& nums, int target) {
         
-        int lo = 0;
-        int hi = nums.size() -1 ;
-        int mid ;
+        int lo = 0 ;
+        int hi = nums.size() -1 , n = nums.size() ;
+        int mid, pivot  ;
         
-        
-        while(lo<hi){
+//         if( n == 1 ){
             
-            //FIRST 
+//             if(nums[0] == target ) 
+//                 return 0 ;
+            
+//             return -1 ;
+//         }
+        
+        while(lo <= hi){
+            
+            mid = lo  + (hi-lo)/2 ;
+            
+           if(nums[mid] <= nums[n-1]) 
+               hi = mid -1 ;
+            
+            else{
+                if(nums[mid] > nums[mid+1] ) 
+                {
+                    pivot = mid ;
+                    break ;
+                }
+                
+                else 
+                    lo = mid + 1 ;
+            }
+        }
+            
+            if(nums[0] <= target && target <= nums[pivot] ) 
+            {
+                lo = 0;
+                hi = pivot ;
+            }
+            else 
+            {
+                lo = pivot +1 ;
+                hi = n-1 ;
+            }
+        
+        while(lo <= hi ){
             
             mid = lo + (hi-lo)/2 ;
             
-            
-            if(nums[nums.size()-1]-nums[mid] >= 0 ) //FFF*TTT   // First T 
-                hi = mid ;
+            if(nums[mid] == target ) 
+                return mid ;
+            else if(nums[mid] > target )
+                hi = mid -1 ;
             else 
-                lo = mid +1 ;   
-            
-            
+                lo = mid +1 ;
         }
-        
-       if(lo == 0)
-           return -1 ;
-        
-        return lo;
-           
-        
-    }
-    
-    int searchNum(vector<int>&nums, int target, int lo, int hi){
-        
-        int mid ;
-        
-        
-        while(lo<hi){
-            
-            //FIRST 
-            
-            mid = lo + (hi-lo)/2 ;
-            
-            
-            if(nums[mid] >= target ) //FFF*TTT   // First T 
-                hi = mid ;
-            else 
-                lo = mid +1 ;   
-            
-            
-        }
-        
-        if(nums[lo] == target)
-            return lo ;
         
         return -1 ;
         
-    }
-    
-    
-    
-    
-    
-        
-    
-    int search(vector<int>& nums, int target) {
-        
-        int pivot = searchPivot(nums);
-        int res  ;
-        
-        cout<<pivot<<endl;
-        
-        if(pivot == -1) 
-            return searchNum(nums,target,0,nums.size()-1);
-        
-        if(nums[pivot] == target)
-            return pivot ;
-        
-        
-        
-        
-        if(nums[nums.size()-1] >= target &&  target >= nums[pivot])
-            //BInary search from pivot to end Index 
-             res = searchNum(nums,target,pivot,nums.size()-1) ;
-        else 
-            //BInary search from  0 to pivot -1 
-            
-             res = searchNum(nums,target,0,pivot-1) ;
-            
-            
-            return res ;
     }
 };

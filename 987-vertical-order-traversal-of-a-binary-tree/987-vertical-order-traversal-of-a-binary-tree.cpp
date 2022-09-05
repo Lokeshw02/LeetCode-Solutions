ea638bae0143949910526231b0c4819ac29551cf
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-05 19:46:37 +0530

    Time: 12 ms (30.19%), Space: 15.6 MB (5.36%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,61 +12,39 @@
 class Solution {
 public:
     
-    void FindAnswer(TreeNode* root, int currentX, int currentY, map<int, vector<pair<int,int>>>& resMap)
-    {
-        // base case - no node, return
-        if(root == NULL)
-            return;
-        
-        // left node recursion
-        FindAnswer(root->left, currentX - 1, currentY - 1, resMap);
+    map<int,map<int,multiset<int>>> m ;
     
-        // if we have a new entry, then insert a new entry point
-        if(resMap.find(currentX) == resMap.end())
-        {
-            // insert it as a new vector with a pair of the Y and our value
-            resMap[currentX] = vector<pair<int,int>>(1, pair<int,int>(currentY, root->val));
-        }
-        else
-        {
-            // insert the new value
-            resMap[currentX].push_back(pair<int,int>(currentY, root->val));
-            
-            // resort the list based on TWO conditions
-            // first: if we have two equal positions, place the smaller one earlier
-            // second: if they are not equal, place the higher Y value first
-            std::sort(resMap[currentX].begin(), resMap[currentX].end(), [](const pair<int,int>& v1, const pair<int,int>& v2) {
-                if(v1.first == v2.first)
-                    return v1.second < v2.second;
-                return v1.first > v2.first;
-            });
-        }
+    void dfs(TreeNode* root, int col , int row){
         
-        // right node recursion
-        FindAnswer(root->right, currentX + 1, currentY - 1, resMap);
+        if(!root) return ;
+        
+        m[col][row].insert(root->val) ;
+        
+        
+        //Left 
+        dfs(root->left,col-1,row+1) ;
+        //Right 
+        dfs(root->right,col+1,row+1) ;
     }
+    
+    
+    
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         
-         vector<vector<int>> res;                    // the answer to return
-        map<int, vector<pair<int,int>>> resMap;     // tracking our data
+        dfs(root,0,0) ;
+        vector<vector<int>>res ;
         
-        FindAnswer(root, 0, 0, resMap);             // calculate answer
-        
-        // iterate over our tracked data
-        for(pair<int, vector<pair<int,int>>> x : resMap)
-        {
-            // create a new vector to add to our res
-            vector<int> resPartial;
-            
-            //add each actual value to our vector instead of the index
-            for(pair<int,int> partial : x.second)
-                resPartial.push_back(partial.second);
-            
-            // add it to our res
-            res.push_back(resPartial);    
+        for(auto t1 : m){
+            res.push_back(vector<int>()) ;
+            for(auto t2 : t1.second )
+            {
+                for(auto t3 : t2.second)
+                    res.back().push_back(t3) ;
+            }
         }
         
-        return res;
+        
+        return res ;
         
     }
 };

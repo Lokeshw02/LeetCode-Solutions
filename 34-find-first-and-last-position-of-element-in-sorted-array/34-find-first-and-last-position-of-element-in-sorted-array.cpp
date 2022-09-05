ae34aadbcb706232a20deee0034ca4d1e717e1b2
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-06 00:06:37 +0530

    Time: 18 ms (24.94%), Space: 13.7 MB (17.94%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,56 +2,71 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         
-  
-        int n = nums.size() ;
-        int lo = 0, hi = n-1, mid, firstPos, endPos ;
-        
-            if (n==0) return {-1,-1}  ;
-        
-        while(lo<hi){
+        int lo = 0 ;
+        int hi = nums.size() -1 ;
+        int mid ;
+        int p1,p2 ;
+        //FIrst Postion 
+        while(lo <= hi){
             
-            mid = lo+ (hi-lo)/2 ;
+            mid = lo + (hi-lo)/2 ;
+            
+            if(nums[mid] > target )
+                hi = mid -1 ;
+            else if(nums[mid] < target )
+                lo = mid +1 ;
+            else {
                 
-             if(nums[mid]>=target)
-                 hi = mid ;
-             else
-                 lo = mid +1 ;
-                  
-                 
-            
-        }
-        
-       if(nums[lo] != target) {
-           firstPos = -1 ;
-       }
-        else {
-            firstPos = lo ;
-        }
-        
-        
-         lo = 0, hi = n-1;
-         while(lo<hi){
-            
-            mid = lo+ (hi-lo+1)/2 ;
+                if(mid == 0){
+                    p1 = mid ;
+                    break ;
+                } 
                 
-             if(nums[mid]>target)
-                 hi = mid-1 ;
-             else
-                 lo = mid ;
-                  
-                 
+                if(nums[mid-1] != target )
+                    { p1 = mid ;
+                    break ;}
+                else 
+                    hi = mid -1 ;
+            }
+                
+        }
+        
+        if(lo>hi) return {-1,-1} ;
+        
+        else p1 = mid ;
+        
+        //Last Occurance 
+        
+        lo = 0, hi = nums.size() -1 ;
+        
+        while(lo <= hi){
             
+            mid = lo + (hi-lo)/2 ;
+            
+            if(nums[mid] > target )
+                hi = mid -1 ;
+            else if(nums[mid] < target )
+                lo = mid +1 ;
+            else {
+                
+                if(mid == nums.size() -1) {
+                    p2 = mid ;
+                    break ;
+                }
+                
+                if(nums[mid+1] != target )
+                   { p2 = mid ;
+                    break ;}
+                else 
+                    lo  = mid + 1 ;
+            }
+                
         }
         
-        if(nums[lo] != target) {
-           endPos = -1 ;
-       }
-        else {
-            endPos = lo ;
-        }
         
         
-        return {firstPos,endPos} ;
+        return {p1,p2} ;
+        
         
     }
 };

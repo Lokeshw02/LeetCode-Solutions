7786c682af22307eab3a0f52d54de468e6c9aeec
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-21 14:50:25 +0530

    Time: 12 ms (69.31%), Space: 18.7 MB (99.12%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -15,30 +15,6 @@ public:
         
         if(!root) return 0 ;
         
-        queue<TreeNode*> q ; 
-        
-        q.push(root) ;
-        int level = 0 ;
-        
-        while(!q.empty()){
-            
-            int n = q.size() ;
-            level++ ;
-            
-            for(int i= 0 ; i < n ; i++){
-                
-                TreeNode* t = q.front() ;
-                q.pop() ;
-                
-                if(t->left != NULL ) q.push(t->left) ;
-                if(t->right != NULL) q.push(t->right) ;
-                
-            }
-            
-            
-            
-        }
-        
-        return level ;
+        return  1+ max(maxDepth(root->left), maxDepth(root->right) ) ;
     }
 };

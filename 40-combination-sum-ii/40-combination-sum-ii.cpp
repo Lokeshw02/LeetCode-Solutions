7d805799bcc89c0cec58444e03a7212308d9fd96
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-22 14:35:51 +0530

    Time: 11 ms (53.25%), Space: 10.6 MB (89.34%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,30 +1,45 @@
 class Solution {
 public:
-    set<vector<int>>res;
-    vector<int>curr;
-    void solve(int i, vector<int>&nums, int target){
-        int n = nums.size();
-        if(target==0){
-            res.insert(curr);
-            return;
-        }    
-        if(target<0)
-            return;
-        if(i==n)
-            return;
-        curr.push_back(nums[i]);
-        solve(i+1, nums, target-nums[i]);
-        curr.pop_back();
-        int j=i+1;
-        while(j<n && nums[j]==nums[i] )
-            j++;
-        solve(j, nums, target);
+    
+    void helper (vector<int>&nums, vector<int>&temp,vector<vector<int>> &res,int target,int i ){
+        
+        
+        if(target == 0 ) 
+        {
+            res.push_back(temp) ;
+            return ;
+        }
+        
+        if(i >= nums.size() ) return ;
+        
+        if(target < 0 )return ;
+        
+        
+        //Include 
+        temp.push_back(nums[i]) ;
+        helper(nums,temp,res, target - nums[i], i+1) ;
+        
+        
+        temp.pop_back() ; 
+        
+        int prev = nums[i],j;
+        
+        for(j=i;j<nums.size() && nums[j]==prev;j++);
+        
+ 
+        helper(nums,temp,res,target,j) ;
+
     }
-    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
-        int n = nums.size();
-       // cout << n<<endl;
-        sort(nums.begin(), nums.end());
-        solve(0, nums, target);
-        return vector<vector<int>>(res.begin(), res.end());
+    
+    
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+         vector<int> temp ;
+        vector<vector<int>> res ; 
+        sort(candidates.begin(), candidates.end()) ;
+        
+        helper(candidates, temp, res, target,0) ;
+        
+        return res ;
     }
 };

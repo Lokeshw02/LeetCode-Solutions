5aa00b5eb23f24f12681eb5306dd8334452514ed
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-22 15:55:58 +0530

    Time: 8 ms (32.68%), Space: 7.7 MB (55.34%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
     
-    void helper(vector<int>& nums, vector<vector<int>>&res,vector<int> &temp,vector<int> &visited ){
+    void helper(vector<int>&nums,  vector<int>& visited , vector<int>&temp, vector<vector<int>> &res  ){
         
-        if(temp.size() == nums.size()) 
-        {
+        if(temp.size() == nums.size()){
             res.push_back(temp) ;
             return ;
         }
         
-        for(int i = 0;i< nums.size() ; i++){
-            
-            if(visited[i] == 0) {
-                visited[i] = 1 ;
-                 temp.push_back(nums[i]) ;
-                 helper(nums,res,temp,visited) ;
-                visited[i] = 0 ;
-                temp.pop_back() ;
-                
-            }
-               
-        }
+        
+       for(int i = 0 ; i < nums.size() ; i++){
+           
+           if(visited[i] == 0){
+            visited[i] = 1 ;
+           temp.push_back(nums[i]) ;
+           helper(nums,visited, temp, res) ;
+           
+           visited[i] = 0 ;
+           temp.pop_back() ;
+           }
+           
+          
+       }
         
         
     }
-    
-    
-    
+ 
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>res ;
-        vector<int> temp ;
-        vector<int> visited(nums.size(),0) ; 
+      vector<int>temp ;
+      vector<vector<int>> res ;
+      vector<int> visited(nums.size(),0) ;
         
-        helper(nums,res,temp,visited) ;
+       helper(nums,visited,temp,res) ;
         
         return res ;
-        
         
     }
 };

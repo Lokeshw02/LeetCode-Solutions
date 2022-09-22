8f15aed3a8c8920d04b304e9289691f1bd204cec
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-22 16:05:58 +0530

    Time: 6 ms (90.88%), Space: 8.4 MB (96.39%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,52 +1,44 @@
 class Solution {
 public:
     
-     void helper(vector<int>&arr, vector<int>&visited, vector<int>&currSet, vector<vector<int>>&res){
-         if(currSet.size() == arr.size()){
-             res.push_back(currSet) ;
-             return ;
-         }
-         
-         for(int i = 0 ;i<arr.size();i++){
-             
-             if(visited[i] == 0){
-                 
-                 visited[i] = 1 ;
-                 currSet.push_back(arr[i]) ;
-                 helper(arr,visited,currSet,res) ;
-                 visited[i] = 0 ;
-                 currSet.pop_back() ;
-                 
-//                    while(i<arr.size()-1 && arr[i] == arr[i+1]) i++ ;
-                 
-                    int j ;
-                    
-                 for(j =i ; j<arr.size()-1 && arr[j] == arr[j+1];j++) ;
-                 i = j ;
-                 
-             }
-             
-          
-              
-             
-             
-         }
-             
-         
-         
-         
-         
-     }
     
+    void helper(vector<int>&nums,  vector<int>& visited , vector<int>&temp, vector<vector<int>> &res  ){
+        
+        if(temp.size() == nums.size()){
+            res.push_back(temp) ;
+            return ;
+        }
+        
+        
+       for(int i = 0 ; i < nums.size() ; i++){
+           
+           if(visited[i] == 0){
+            visited[i] = 1 ;
+           temp.push_back(nums[i]) ;
+           helper(nums,visited, temp, res) ;
+           
+           visited[i] = 0 ;
+           temp.pop_back() ;  
+               
+           int prev = nums[i], j ;
+               
+            for(j = i ;j < nums.size()-1 && nums[j] == nums[j+1]; j++){} ;
+               
+               i = j ;
+           }
+           
+       }
+    }
+   
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         
         sort(nums.begin(),nums.end());
         vector<vector<int>> res;
-        vector<int>currSet;
+        vector<int>temp;
         vector<int> visited(nums.size(),0);
-        helper(nums,visited,currSet,res);
+        helper(nums,visited,temp,res);
         
-        return res  ;
+        return res ;
         
     }
 };

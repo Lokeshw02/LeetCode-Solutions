2a784b94627c749a9f8e4a595fdd686079792a59
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-22 21:03:10 +0530

    Time: 51 ms (15.08%), Space: 11.4 MB (30.04%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res = "" ; 
+        string temp = ""; 
+        
+        for(int i = 0 ; i < s.size() ; i++){
+            
+             if(s[i] != ' ')
+             temp += s[i] ; 
+             
+             else{
+                 reverse(temp.begin(), temp.end()) ;
+                 res += temp ;
+                 res+= " " ;
+                 temp = "" ;
+             }
+             
+            
+           
+        }
+        
+         reverse(temp.begin(), temp.end()) ; 
+        res += temp ;
+          
+        
+        
+        return res ;
+    }
+};

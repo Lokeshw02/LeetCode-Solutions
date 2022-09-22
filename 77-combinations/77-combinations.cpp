68e9e6b69c63a519a6685400e8b44120c10ca2d8
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-22 12:12:45 +0530

    Time: 7 ms (99.07%), Space: 18 MB (38.45%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
     
-    
-    void helper(int startIdx,int n, int k,vector<int>&currSet,vector<vector<int>>&res){
+    void helper(int n, int k, int i,vector<int> &temp, vector<vector<int>> &res ){
         
-        if(k==0) {
-            res.push_back(currSet) ;
+        if(k  == 0 ){
+            res.push_back(temp) ;
             return ;
         }
         
-        if(startIdx>n) 
+        if(i > n) 
+            return  ;
+        
+        if(n-i +1 < k ) 
             return ;
         
-        if(n-startIdx+1 < k) return ;
         
-        //Include 
-        currSet.push_back(startIdx) ;
         
-        helper(startIdx+1,n,k-1,currSet,res) ;
-        
-        currSet.pop_back() ;
+        //Include
+        temp.push_back(i) ;
+        helper(n,k-1,i+1,temp,res) ;
+        temp.pop_back() ;
         
         //Exclude 
         
-        helper(startIdx+1,n,k,currSet,res) ;
-        
-        
-        
+        helper(n,k, i+1,temp, res ) ;
         
     }
     
     
     
     
-    
-    
     vector<vector<int>> combine(int n, int k) {
-    
-         vector<int>currSet ;
-         vector<vector<int>>res ;
-         
-        helper(1,n,k,currSet,res) ;
+        vector<vector<int>> res ;
+        vector<int> temp ;
+        
+        helper(n,k,1,temp,res) ;
         
         return res ;
-        
-        
     }
 };

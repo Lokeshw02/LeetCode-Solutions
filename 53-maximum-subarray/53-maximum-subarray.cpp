7bf4d44a12399b0ef80148669ad4a9d6c335399c
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-24 02:27:37 +0530

    Time: 322 ms (6.63%), Space: 67.8 MB (10.84%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,19 +4,15 @@ public:
         
         int n = nums.size() ;
         
-       int prev = nums[0] ;
-       int res = prev ;
-       int curr;
+        int prev = nums[0], curr ;
         
+        int res = nums[0] ;
         
-        for(int i = 1; i<n; i++){
-         
-        curr = max(nums[i],nums[i]+prev) ;
-        res = max(res,curr) ;
-        
-        prev = curr ;
+        for(int i = 1 ; i< n ; i++){
+            curr = max(nums[i], prev+nums[i]) ;
+            res = max(curr,res) ; 
+            prev = curr ;
         }
-        
         return res ;
     }
 };

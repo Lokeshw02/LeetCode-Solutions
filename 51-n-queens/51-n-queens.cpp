8aaf6183cdd9ff407bb920b05f7f6f7983ebd7e3
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-23 12:35:38 +0530

    Time: 6 ms (83.13%), Space: 7.3 MB (66.63%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,49 +1,53 @@
 class Solution {
 public:
     
-    void helper(vector<bool>&colV,vector<bool>&rowV,vector<bool>&pdV,vector<bool>&sdV,int n,int rowNum,vector<string>& currBoard,vector<vector<string>>&res ){
-        
-        if(rowNum == n ) {
-            res.push_back(currBoard) ;
-            return ;
-        }
+    void helper(int n, int rowNum, vector<bool>&rowV,vector<bool>&colV,vector<bool>&pdV,vector<bool>&sdV, vector<string> &currboard,vector<vector<string>> &res){
         
         int col ;
         
-        for(col =0; col<n;col++){
-            if(rowV[rowNum]||colV[col]||pdV[rowNum-col+n-1]||sdV[rowNum+col])
-                continue ;
-            
-            rowV[rowNum] = true;
-            colV[col]= true;
-            pdV[rowNum-col+n-1]= true;
-            sdV[rowNum+col]= true;
-            
-            currBoard[rowNum][col] = 'Q' ;
-            
-            helper(colV,rowV,pdV,sdV,n,rowNum+1,currBoard,res) ;
-            
-            currBoard[rowNum][col] = '.' ;
-            
-             sdV[rowNum+col]= false;
-             pdV[rowNum-col+n-1]= false;
-             colV[col]= false;
-             rowV[rowNum] = false;
+        if(rowNum == n ){
+            res.push_back(currboard) ;
+            return ;
         }
+        
+        for(col = 0 ; col < n ; col++){
+            if(rowV[rowNum] || colV[col] || pdV[rowNum - col + n -1 ] || sdV[rowNum + col] ) 
+                continue  ;
+            
+            rowV[rowNum] = true ;
+            colV[col] = true ;
+            pdV[rowNum - col + n -1 ] = true ;
+            sdV[rowNum + col] = true ;
+            
+            currboard[rowNum][col] = 'Q' ;
+            
+            helper(n,rowNum+1,rowV,colV,pdV,sdV,currboard,res) ;
+            
+            currboard[rowNum][col] = '.' ;
+            
+            sdV[rowNum + col] = false ;
+            pdV[rowNum - col + n -1 ] = false ;
+            colV[col] = false ;
+            rowV[rowNum] = false ;
+           
+            
+        }
+        
+        
     }
     
     
+    
     vector<vector<string>> solveNQueens(int n) {
-        
-        vector<bool>colV(n,false) ;
         vector<bool>rowV(n,false) ;
-        vector<bool>pdV(2*n -1,false) ;
-        vector<bool>sdV(2*n -1,false) ;
-        vector<string>currBoard(n,string(n,'.')) ;
+         vector<bool>colV(n,false) ; 
+         vector<bool>pdV(2*n-1,false) ; 
+         vector<bool>sdV(2*n-1,false) ;  
+        
+        vector<string> currboard(n,string(n,'.')) ;  
         vector<vector<string>> res ;
         
-         helper(colV,rowV,pdV,sdV,n,0,currBoard,res) ;
-             
+         helper(n,0,rowV,colV,pdV,sdV,currboard,res) ; 
         
         return res ;
     }

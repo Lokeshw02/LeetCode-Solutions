0fdaed1dea3639e642c16f2cf71ed367220ebb5b
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-24 00:33:02 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (26.00%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
     
-    int robhelper(vector<int>&nums,vector<int>&dp,int startIdx){
+    
+    int helper(vector<int>& nums,vector<int>&dp, int i){
         
-        
-        
-        
-        if(startIdx >= nums.size())
+        if(i >= nums.size())
             return 0 ;
         
+        if(dp[i] != -1) 
+            return dp[i] ;
         
-        if(dp[startIdx] != -1) return dp[startIdx] ;
+        //Include 
+        int amount1 = nums[i] + helper(nums,dp,i+2) ;
         
+        int amount2 = helper(nums,dp,i+1) ;
         
-        //Thief starts from the startIdx house 
-        
-       int  m1 = nums[startIdx]+ robhelper(nums,dp,startIdx+2) ;
-            
-        // If it does not include startIdx
-        
-       int  m2 = robhelper(nums,dp,startIdx+1);
-        
-            
-        dp[startIdx] = max(m1,m2) ;
-        
-        return dp[startIdx] ;
-        
+        return dp[i] = max(amount1,amount2) ;
         
         
     }
-      
     
-    
+   
     int rob(vector<int>& nums) {
+        int n = nums.size() ;
         
-        vector<int>dp(nums.size(),-1) ;
+        vector<int>dp(n,-1) ;
+        //0 to n-1  
         
-        return robhelper(nums,dp,0) ;
+        return helper(nums,dp,0) ;
+        
+        
         
         
     }

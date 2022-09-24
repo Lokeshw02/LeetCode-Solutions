e3349fe7f95fb484046360b90e7aaa83a31e48e5
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-25 00:59:27 +0530

    Time: 14 ms (23.51%), Space: 11.2 MB (77.64%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size() ;
+          int n = prices.size() ;
+        
+        vector<int>dp1(n,0) ;//Last was buy 
+        vector<int>dp2(n,0) ;//Last was sell 
         
         if(n<=1) return 0 ;
         
-        vector<int> dp1(n,0) ;
-        vector<int>dp2(n,0) ;
-        
         dp1[0] = -prices[0] ;
-        
         dp2[0] = 0 ;
-       
-        //Maximum Profit you can make till i th day considering that the last transaction in Buy 
-        dp1[1] = max(-prices[0],-prices[1]) ;
-        ////Maximum Profit you can make till i th day considering that the last transaction in Sell  
-        dp2[1] = max(0, prices[1] - prices[0]) ;
         
-        for(int i = 2; i<n ;i++){
+        dp1[1] = max(-prices[0],-prices[1]) ;
+        dp2[1] = max(-prices[0]+prices[1],0) ;
+        
+        for(int i = 2; i < n ; i++){
             
-            dp1[i] = max(dp1[i-1],dp2[i-2]- prices[i]) ;
+            dp1[i] = max(dp1[i-1],dp2[i-2] - prices[i]) ;
             dp2[i] = max(dp2[i-1], dp1[i-1]+prices[i]) ;
         }
         
-        return dp2[n-1];
+        return dp2[n-1] ;
+        
+        
     }
-    
 };

a993fbd13246e5b6104d7a5078089152275f069c
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-24 19:48:35 +0530

    Time: 11 ms (86.80%), Space: 19.9 MB (60.91%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -11,34 +11,37 @@
  */
 class Solution {
 public:
-    vector<vector<int>> v;
-    vector<int> temp;
-    int sum=0;
     
-    void helper(TreeNode *root,int sum, int targetSum,vector<int> temp){
-        if(root==NULL){
-            return ;
+    void helper(TreeNode* root, int targetSum, vector<int>&curr, vector<vector<int>>&res){
+        
+        
+        if(!root) return ;
+        
+         curr.push_back(root->val) ;
+        
+        if(root->left == NULL && root->right == NULL ){
+            
+            if(targetSum == root->val)
+            res.push_back(curr) ;
+        
         }
         
-        sum+=root->val;
-        temp.push_back(root->val);
+      
         
-        if(!root->left && !root->right){
-            if(sum==targetSum){
-                v.push_back(temp);
-            }
-            else{
-                return;
-            }
-        }
+        helper(root->left,targetSum - root->val, curr, res) ; 
         
-        helper(root->left,sum,targetSum,temp);
-        helper(root->right,sum,targetSum,temp);
+        helper(root->right,targetSum - root->val, curr, res) ;
+        
+        curr.pop_back() ;
+        
     }
-    
+   
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        helper(root,0,targetSum,temp);
-        return v;
+        vector<vector<int>>res ;
+        vector<int> curr ;
         
+        helper(root, targetSum, curr, res) ;
+        
+        return res ;
     }
 };

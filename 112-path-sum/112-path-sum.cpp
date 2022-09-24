1c3657d484957ede365137d4dc8d2e104b8158e9
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-24 19:28:37 +0530

    Time: 23 ms (31.52%), Space: 21.1 MB (92.08%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -12,16 +12,12 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        //Base condition 
         
-        if(root == NULL) return false ;
+        if(!root) return false  ;
         
-        //True Condition --> Leaf Node and Sum Becomes 0 
+        if(root->left == NULL && root->right == NULL && targetSum - root->val == 0 ) 
+            return true ;
         
-        if(root->left ==NULL && root->right ==  NULL && targetSum-root->val ==0) return true ;
-        
-        //Chek if the value exists in the left and right Subtree 
-        
-        return hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val) ;
+        return hasPathSum(root->left,targetSum- root->val) || hasPathSum(root->right,targetSum- root->val) ;
     }
 };

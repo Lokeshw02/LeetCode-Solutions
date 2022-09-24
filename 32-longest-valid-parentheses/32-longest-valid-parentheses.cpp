15eeae027d25e36d481ac9d1a3fb4ef0a40d0113
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-24 22:36:48 +0530

    Time: 11 ms (28.66%), Space: 7.5 MB (19.72%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-         
-        int n = s.size() ,i , res = 0 , L ;
+        int n = s.size(), i, L ;
+        vector<int>dp(n,0 ) ;
+        int res = INT_MIN ;
         
-        if(n==0) return 0 ;
-        
-        vector<int> dp (n, 0) ;
+        if(n==0 || n == 1) return 0 ;
         
         dp[0] = 0 ;
         
-        for(i = 1; i< n;i++){
+        for(i = 1 ; i < n ; i++){
             
-               
-               if(s[i] == ')'){
-               
+            if(s[i] == '(') 
+                dp[i] = 0 ;
+            else
+            {
+                L = dp[i-1] ; 
                 
-                if(i-dp[i-1]-1 >= 0 && s[i-dp[i-1]-1] == '('){
-                   
-               
+                if(i-L-1 >= 0 && s[i-L-1] == '(') {
                     
-                    dp[i] = 2 + dp[i-1] ;
+                dp[i] = 2 + dp[i-1] ;
                     
-                    if(i-dp[i-1]-2 >= 0) 
-                        dp[i] += dp[i-dp[i-1]-2] ;
+                if(i-L-2 >=0)
+                dp[i] += dp[i-L-2] ;
                 }
+                   
             }
             
             res = max(res,dp[i]) ;

5b177f6c06d8a1b90e4ee3585ec27dc310f5b209
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-25 07:29:40 +0530

    Time: 244 ms (15.00%), Space: 5.9 MB (95.68%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        
+        string s = "" ;
+        
+        for(int i = 1; i <= n ;i++ ){
+            s += to_string(i) ;
+        }
+        
+        while(--k){
+            
+            bool t = next_permutation(s.begin(),s.end()) ;
+        }
+        
+        return s ;
+    }
+};

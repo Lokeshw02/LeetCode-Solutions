efa99d73def5b2de8d6bca447e9cb254844359e9
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-26 09:03:48 +0530

    Time: 408 ms (22.99%), Space: 77.3 MB (67.77%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int> stk ;
         int n = heights.size() ;
-        int area_top, max_area = 0;
-        int tp ;
-        
-        int i = 0 ;
+        int max_area = 0, area_top = 0 ;
+        stack<int> stk ;
+        int i, tp ;
+        i = 0 ;
         
         while(i<n){
             
-            if(stk.empty() || heights[stk.top()] <= heights[i] ){
-                 stk.push(i) ; 
-                 i++ ;
+            if(stk.empty() || heights[stk.top()] <= heights[i]) {
+               stk.push(i) ;
+                i++ ;
             }
-            
             else{
+                
                 tp = stk.top() ;
                 stk.pop() ;
                 
-                //Calculate area
                 if(!stk.empty())
-                area_top =  heights[tp]*(i - stk.top()-1) ;
+                area_top = heights[tp]*(i - stk.top() -1) ; 
                 else
                     area_top = heights[tp]*i ;
                 
                 max_area = max(max_area,area_top) ;
                 
             }
-                   
+                
+            
         }
         
         while(!stk.empty()){
@@ -36,11 +35,12 @@ public:
             stk.pop() ;
             
             if(!stk.empty())
-                area_top = heights[tp]*(i-stk.top()-1);
-            else
-                 area_top = heights[tp]*i ; 
-            
+                area_top = heights[tp]*(i - stk.top() -1) ; 
+                else
+                    area_top = heights[tp]*i ;
+                
             max_area = max(max_area,area_top) ;
+            
         }
         
         return max_area ;

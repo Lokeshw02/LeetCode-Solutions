b82e38b3c27c93ab8f00cc79a3d038be956ea678
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-27 18:17:42 +0530

    Time: 25 ms (85.60%), Space: 13.5 MB (74.57%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,42 +1,41 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-      
-        vector<vector<int>>graph(numCourses) ;
+        int n = numCourses ;
+        vector<vector<int>>graph(n) ;
+        vector<int>indegree(n,0) ;
+        vector<int> res ;
+        int count = 0 ;
         
-        vector<int>indegrees(numCourses,0) ;
-        
-        vector<int> res  ;
-        int count = 0  ;
-        
-        //Explicit graph creation 
-        for(int i = 0 ; i< prerequisites.size() ; i++){
-            graph[prerequisites[i][1]].push_back(prerequisites[i][0]) ;
-            indegrees[prerequisites[i][0]]++ ;
+        for(int i = 0; i < prerequisites.size() ; i++){
+            graph[prerequisites[i][1]].push_back(prerequisites[i][0]) ; 
+            indegree[prerequisites[i][0]]++ ;
         }
-        
         
         queue<int>q ;
         
-        for(int i = 0; i < indegrees.size() ; i++){
-            if(indegrees[i] == 0)
-                q.push(i) ;
+        for(int i = 0; i< n ; i++){
+         if(indegree[i] == 0) 
+             q.push(i) ;
         }
         
         while(!q.empty()){
-            int t = q.front()  ;
+            int t = q.front() ;
             q.pop() ;
-            count++ ;
-            res.push_back(t) ;
-            //Decrease the Indegrees of all the Adjecent  Nodes 
             
-            for(auto &nbr : graph[t]){
-                indegrees[nbr]-- ;
-                if(indegrees[nbr] == 0)
+            res.push_back(t) ;
+            count++ ;
+            
+            for(int nbr : graph[t]){
+                indegree[nbr]-- ;
+                
+                if(indegree[nbr] == 0)
                     q.push(nbr) ;
             }
-          
+            
+            
         }
-       return count == numCourses ? res : vector<int>() ;
+        
+        return count == n ? res : vector<int>() ;
     }
 };

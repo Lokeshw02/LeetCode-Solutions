673480d9926bbf95818fa4c2079ab32d246adbb2
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-27 21:33:59 +0530

    Time: 556 ms (22.96%), Space: 90.2 MB (58.63%) - LeetHub

--- 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.cpp
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.cpp
@@ -1,49 +1,68 @@
-typedef long long ll;
 class Solution {
-   public:
-    // get sum of subarray using prefix sum array
-    ll getSumRange(vector<ll>& vec, int l, int r) {
-        if (l == -1) return vec[r - 1];
-        return vec[r - 1] - vec[l];
-    }
+public:
+     
     int maxSumMinProduct(vector<int>& nums) {
-        int n = nums.size();
-        stack<int> s;
-        //find index of nearest smaller element to the left
-        vector<int> left(n, -1);
-        for (int i = 0; i < n; i++) {
-            while (!s.empty() && nums[s.top()] >= nums[i]) {
-                s.pop();
+        
+        stack<int> stk ;
+        int n = nums.size() ;
+        
+        //Genereate nearest smaller to the left{index} 
+        vector<int>left_nearest(n,-1) ;
+        
+        for(int i = 0; i < n ; i++){
+            
+            while(!stk.empty() && nums[stk.top()] >= nums[i] ){
+                stk.pop() ;
             }
-            if (!s.empty())
-                left[i] = s.top();
-            s.push(i);
+            
+            if(!stk.empty()) 
+                left_nearest[i] = stk.top();
+            
+            stk.push(i) ;    
+           }
+        
+        //Clear stack 
+         while(!stk.empty()) stk.pop() ;
+        
+        //Genrate nearest smaller to right{index} 
+        
+        vector<int>right_nearest(n,n) ;
+        
+        for(int i = n-1; i>=0 ; i--){
+            while(!stk.empty() && nums[stk.top()] >= nums[i]) 
+                stk.pop() ;
+            
+           if (!stk.empty())  
+               right_nearest[i] = stk.top() ;
+            
+            stk.push(i) ;
+                
         }
-        //clear stack
-        while (!s.empty()) s.pop();
-        //find index of nearest smaller element to the right
-        vector<int> right(n, n);
-        for (int i = n - 1; i >= 0; i--) {
-            while (!s.empty() && nums[s.top()] >= nums[i]) {
-                s.pop();
-            }
-            if (!s.empty())
-                right[i] = s.top();
-            s.push(i);
-        }
-        //generate prefix array
-        vector<ll> prefix(n);
-        for (int i = 0; i < n; i++)
-            if (i == 0)
+        //Generate Prefix Array 
+        vector<long long > prefix(n) ;
+        
+        for(int i = 0; i< n; i++){
+            if(i==0) 
                 prefix[i] = nums[i];
             else
-                prefix[i] = nums[i] + prefix[i - 1];
-        //generate answer
-        ll ans = 0;
-        for (int i = 0; i < n; i++) {
-            ll val = (ll)nums[i] * (ll)(getSumRange(prefix, left[i], right[i]));
-            ans = max(ans, val);
+                prefix[i] = prefix[i-1] + nums[i] ;
         }
-        return int(ans % int(1e9 + 7));
+        
+        //Generate ANswers 
+        
+        long long ans = 0 ;
+        
+        for(int i = 0; i < n ; i++){
+            
+            int left = left_nearest[i]  ; 
+            int right = right_nearest[i]  ; 
+            
+            ans = max(ans, (long long) nums[i]*(prefix[right-1] - (left >=0 ? prefix[left] :0))) ;
+            
+            
+        }
+        
+        return int(ans % int(1e9 + 7)) ;
+      
     }
 };

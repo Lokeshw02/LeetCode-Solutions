1ad7d987d72dbdd6829bff398a92def75ff26c57
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-28 18:49:03 +0530

    Time: 427 ms (19.96%), Space: 77.3 MB (62.79%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        int i = 0 ;
+        stack<int> stk;
+        int max_area = -1 , area_top;
         int n = heights.size() ;
-        int max_area = 0, area_top = 0 ;
-        stack<int> stk ;
-        int i, tp ;
-        i = 0 ;
+        int tp ;
         
         while(i<n){
             
-            if(stk.empty() || heights[stk.top()] <= heights[i]) {
-               stk.push(i) ;
+            if(stk.empty() || heights[stk.top()] <= heights[i]){
+                stk.push(i) ;
                 i++ ;
-            }
-            else{
                 
+            }
+            
+            else{
                 tp = stk.top() ;
                 stk.pop() ;
                 
-                if(!stk.empty())
-                area_top = heights[tp]*(i - stk.top() -1) ; 
+                if(stk.empty()) 
+                    area_top = heights[tp]*i ; 
                 else
-                    area_top = heights[tp]*i ;
+                    area_top = heights[tp]*(i - stk.top() - 1)  ;
                 
-                max_area = max(max_area,area_top) ;
+                max_area = max(area_top,max_area) ;
                 
             }
-                
-            
         }
         
         while(!stk.empty()){
             tp = stk.top() ;
-            stk.pop() ;
+            stk.pop();
             
-            if(!stk.empty())
-                area_top = heights[tp]*(i - stk.top() -1) ; 
-                else
-                    area_top = heights[tp]*i ;
-                
-            max_area = max(max_area,area_top) ;
+            if(stk.empty()) 
+                area_top = heights[tp]*i ;
+            else
+                area_top = heights[tp]*(i-stk.top()-1) ; 
             
+            max_area = max(area_top,max_area) ;
         }
         
         return max_area ;
+        
     }
 };

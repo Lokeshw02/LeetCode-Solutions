5577ad11f0978ad248bf612c601a492c91796093
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-28 22:29:07 +0530

    Time: 312 ms (51.55%), Space: 77.2 MB (73.89%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -7,38 +7,37 @@ public:
         int n = heights.size() ;
         int tp ;
         
-        while(i<n){
-            
-            if(stk.empty() || heights[stk.top()] <= heights[i]){
-                stk.push(i) ;
-                i++ ;
-                
-            }
-            
-            else{
-                tp = stk.top() ;
-                stk.pop() ;
-                
-                if(stk.empty()) 
-                    area_top = heights[tp]*i ; 
-                else
-                    area_top = heights[tp]*(i - stk.top() - 1)  ;
-                
-                max_area = max(area_top,max_area) ;
-                
-            }
-        }
+      while(i<n){
+          
+          if(stk.empty() || heights[stk.top()] <= heights[i]) {
+              stk.push(i) ;
+              i++ ;
+           }
+          else{
+              tp = stk.top() ;
+              stk.pop() ;
+              
+              if(stk.empty()) 
+                  area_top = heights[tp]*i ;
+              else
+                  area_top = heights[tp]*(i - stk.top() -1);
+              
+               max_area = max(max_area,area_top) ;
+          }
+             
+         
+      }
         
         while(!stk.empty()){
             tp = stk.top() ;
-            stk.pop();
+            stk.pop() ;
             
             if(stk.empty()) 
                 area_top = heights[tp]*i ;
             else
-                area_top = heights[tp]*(i-stk.top()-1) ; 
+                area_top = heights[tp]*(i- stk.top()-1) ;
             
-            max_area = max(area_top,max_area) ;
+            max_area = max(max_area,area_top) ;
         }
         
         return max_area ;

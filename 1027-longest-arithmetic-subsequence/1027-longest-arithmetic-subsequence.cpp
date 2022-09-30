8c08aed2fff88b054bff6dfd272aaf39d2aa6ab6
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-30 22:55:12 +0530

    Time: 773 ms (29.75%), Space: 141.3 MB (70.47%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int longestArithSeqLength(vector<int>& nums) {
-        int n = nums.size(),i,j,cd,res = 0;
+        int i, j, n = nums.size(), res,cd ;
         
         vector<vector<int>>dp(n,vector<int>(1001,0)) ;
-        //Order of filling row by row
+    
         
-        for(i = 0;i<n;i++){
+        for(i = 0 ; i < n ; i++){
+            for( j = i-1; j >=0; j-- ){
+                
+                 cd = nums[i] - nums[j] ;
+                
+                if(dp[j][cd+500] == 0) 
+                    dp[i][cd+500] = max(dp[i][cd+500], 2) ;
+                else
+                    dp[i][cd+500] = max(dp[i][cd+500], 1 + dp[j][cd+500]) ;
+                
+                 res = max(res, dp[i][cd+500]) ;
+            }
             
-            for(j = i-1; j>=0;j--){
-            cd = nums[i] - nums[j] ;
-            
-            if(dp[j][cd+500] == 0)
-                dp[i][cd+500] = max(dp[i][cd+500],2) ;
-            else
-                dp[i][cd+500] = max(dp[i][cd+500],1+dp[j][cd+500]) ;
-            
-            res = max(res,dp[i][cd+500]) ;
-        }
+           
         }
         
         return res ;

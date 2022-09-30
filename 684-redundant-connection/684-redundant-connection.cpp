0f7a6b2d7eb1a8fa1887ffe1f592f6b65548b051
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-09-30 18:49:19 +0530

    Time: 21 ms (26.32%), Space: 8.8 MB (70.62%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,58 +1,60 @@
 class ds{
+    private :
+    int n ;
+    vector<int>parent ;
+    
     public :
     
     ds(int n ){
         this->n = n ;
-        //1 indexing dummy 
-        parent.push_back(0) ;
         
-        for(int i =1 ; i<= n ;i++){
-            parent.push_back(i) ;
-        }
+    parent.push_back(0) ;
+    
+    for(int i = 1; i <= n ; i++){
+        parent.push_back(i) ;
+    }
     }
     
     int find(int u){
+        
         while(u != parent[u]){
             u = parent[u] ;
-            
-         
         }
-         return u ;
+        
+        return u ;
     }
     
     void merge(int u, int v){
         int g1 = find(u) ;
         int g2 = find(v) ;
         
-        parent[g2] = g1 ;
-        
-       
+        parent[v] = u ;
     }
+   
     
-     private :
-        
-        int n ;
-        vector<int>parent ;
 };
 
 
 
-
 class Solution {
-public:
+public:   
+    
+  
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size() ;
-        ds uf(n) ;
         
-        for(int i =0 ; i< n ; i++){
-            int g1 = uf.find(edges[i][0]) ;
-            int g2 = uf.find(edges[i][1]) ;
-            
-            if(g1 == g2 ) return edges[i] ;
-            
-            uf.merge(edges[i][0], edges[i][1]) ;
-        }
+       int n = edges.size() ; 
+       ds uf(n) ; 
         
+       for(int i = 0 ; i < n ; i++){
+           
+           int g1 = uf.find(edges[i][0]) ;
+           int g2 = uf.find(edges[i][1]) ;
+           
+           if(g1 == g2 ) 
+               return edges[i] ;
+           uf.merge(g1,g2) ;
+       } 
+         
         return {} ;
     }
 };

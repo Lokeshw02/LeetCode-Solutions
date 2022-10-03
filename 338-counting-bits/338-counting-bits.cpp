a705d8f27828cdcf0301afd9ae6ae2551f6745aa
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-10-03 13:21:11 +0530

    Time: 10 ms (50.72%), Space: 8.5 MB (20.89%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,10 +3,16 @@ public:
     vector<int> countBits(int n) {
         vector<int> res ;
         
-        for(int i = 0; i <= n ;i++){
-            int x =  __builtin_popcount(i) ;
-            res.push_back(x) ;
-        } 
+//         for(int i = 0; i <= n ;i++){
+//             int x =  __builtin_popcount(i) ;
+//             res.push_back(x) ;
+//         } 
+        
+        res.push_back(0) ;
+        
+        for(int i = 1; i <= n ;i++){
+            res.push_back(res[i&(i-1)] +1 ) ;
+        }
         return res ;
     }
 };

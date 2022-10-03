15ca175663ca6e29c89c088f92264733f5921496
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-10-03 12:51:52 +0530

    Time: 26 ms (22.76%), Space: 10 MB (80.46%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int totalxor = 0 ;
+        unsigned int totalxor = 0 ;
         int g1= 0, g2 = 0 ;
         
         for(int i = 0 ; i < nums.size() ; i++){
             totalxor ^= nums[i] ;
         }
         
-        int mask = 0 ;
+        unsigned int mask = totalxor&(-totalxor) ; 
         
-        for(int i = 0; i<= 32; i++){
-            if(totalxor&(1<<i)) {
-                mask = mask|(1<<i); 
-                break ;
-            }
+//         for(int i = 0; i<= 32; i++){
+            
+//             if(totalxor&(1<<i)) {
+//                 mask = (1<<i); 
+//                 break ;
+//             }
                 
-        }
+//         }
         
         for(int i = 0 ; i < nums.size() ; i++){
             if(nums[i]&mask) 

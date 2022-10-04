b2baf3d211064724a4666e91c672aed2b8341495
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-10-04 10:46:35 +0530

    Time: 502 ms (62.57%), Space: 8 MB (48.09%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -3,44 +3,46 @@ public:
     
     vector<vector<int>> dirs = {{0,1},{1,0},{-1,0},{0,-1}} ;
     
-    bool helper(vector<vector<char>>& board, string &word, int i, int j, int count ){
-        if(count == word.size()) {
+    bool dfs(vector<vector<char>>& board, string &word, int i, int j, int count){
+        if(count == word.size()) 
             return true ;
-        }
         
-        if(i <0 || j <0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[count]) 
+        bool t = false  ;
+        
+        if(i <0 || i >= board.size() || j <0 || j >= board[0].size() || word[count] != board[i][j])
             return false ;
         
         char temp = board[i][j] ;
         
-        board[i][j] = '*' ;
+         temp = board[i][j] ;
         
-        bool found = false ;
-        
-        for(auto &dir : dirs ){
+          board[i][j] = '*' ;
+         
+        for(auto & dir : dirs ){
             int x = i + dir[0] ;
             int y = j + dir[1] ;
             
-            found = found || helper(board,word,x,y,count+1) ;
+            t = t || dfs(board,word,x,y,count+1) ;
+            
         }
-        
         board[i][j] = temp ;
         
-        
-        return found ;
-        
+        return t ;
     }
     
     
     bool exist(vector<vector<char>>& board, string word) {
+        
         for(int i = 0 ; i < board.size(); i++){
             for(int j = 0 ; j < board[0].size() ; j++){
                 
-                if(board[i][j] == word[0] && helper(board,word,i,j,0)) 
+                if( board[i][j] == word[0] && dfs(board,word,i,j,0) )  
                     return true ;
+                    
+                    
             }
         }
-        
         return false ;
+        
     }
 };

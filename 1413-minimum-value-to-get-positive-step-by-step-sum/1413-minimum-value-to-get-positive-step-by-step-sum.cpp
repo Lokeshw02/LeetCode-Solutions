3458c79a0a41768587c6737c0d9936a0bb4b6aa0
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-10-14 14:41:48 +0530

    Time: 4 ms (41.94%), Space: 7.4 MB (37.09%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,42 +1,53 @@
 class Solution {
 public:
     
-    bool helper(vector<int>& nums, int mid){
+//     bool helper(vector<int>& nums, int mid){
         
-        int min_ = INT_MAX ;
+//         int min_ = INT_MAX ;
         
-        for(int i = 0; i < nums.size() ; i++){
-            mid += nums[i] ;
-            min_ = min(min_,mid) ;
-        }
+//         for(int i = 0; i < nums.size() ; i++){
+//             mid += nums[i] ;
+//             min_ = min(min_,mid) ;
+//         }
         
-        return min_ >= 1 ;
-    }
+//         return min_ >= 1 ;
+//     }
     
     int minStartValue(vector<int>& nums) {
         
-        int lo = 1 , hi = 10000, mid ;
+//         int lo = 1 , hi = 10000, mid ;
         
-        while(lo <= hi){
+//         while(lo <= hi){
             
-            mid = lo + (hi-lo)/2 ;
+//             mid = lo + (hi-lo)/2 ;
             
-            if(helper(nums,mid)) 
-            {
-                if( mid-1 != 0 && !helper(nums,mid-1)) 
-                    return mid ;
-                else 
-                    hi = mid -1 ;
+//             if(helper(nums,mid)) 
+//             {
+//                 if( mid-1 != 0 && !helper(nums,mid-1)) 
+//                     return mid ;
+//                 else 
+//                     hi = mid -1 ;
                     
-            }
+//             }
             
-            else
-            {
-                lo = mid +1 ;
-            }
+//             else
+//             {
+//                 lo = mid +1 ;
+//             }
+//         }
+        
+//         return mid ;
+        int min_ = INT_MAX ;
+        int prefsum = 0 ;
+        
+        for(int i = 0; i < nums.size() ; i++){
+            
+            prefsum += nums[i] ;
+            
+            min_ = min(min_,prefsum) ;
+         
         }
         
-        return mid ;
-        
+        return min_>0 ? 1 : abs(min_) +1 ;
     }
 };

f73856c2960b871b63fcb8cc12edf06d116c6f89
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-10-14 14:28:11 +0530

    Time: 2 ms (61.91%), Space: 7.5 MB (37.09%) - LeetHub

---
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    bool helper(vector<int>& nums, int mid){
+        
+        int min_ = INT_MAX ;
+        
+        for(int i = 0; i < nums.size() ; i++){
+            mid += nums[i] ;
+            min_ = min(min_,mid) ;
+        }
+        
+        return min_ >= 1 ;
+    }
+    
+    int minStartValue(vector<int>& nums) {
+        
+        int lo = 1 , hi = 10000, mid ;
+        
+        while(lo <= hi){
+            
+            mid = lo + (hi-lo)/2 ;
+            
+            if(helper(nums,mid)) 
+            {
+                if( mid-1 != 0 && !helper(nums,mid-1)) 
+                    return mid ;
+                else 
+                    hi = mid -1 ;
+                    
+            }
+            
+            else
+            {
+                lo = mid +1 ;
+            }
+        }
+        
+        return mid ;
+        
+    }
+};

548d9cc621d5a7f2e690134d7953f6e9f3f32fa9
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-10-19 12:57:31 +0530

    Time: 400 ms (9.20%), Space: 132.9 MB (17.24%) - LeetHub

---
+++ 1703-minimum-adjacent-swaps-for-k-consecutive-ones/1703-minimum-adjacent-swaps-for-k-consecutive-ones.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums, int k) {
+        /////
+        long result=2147483647;
+	vector<int> total; //to store indices of all 1s
+	for(int i=0;i<nums.size();i++)
+		if(nums[i]==1) total.push_back(i);
+			
+	int n=total.size();
+	vector<long> s={0}; //to store the prefix sum of the indices of 1s
+	for(int i=0;i<n;i++)
+		s.push_back(s.back()+total[i]);
+
+	for(int i=k/2;i<n-(k-1)/2;i++)
+	{
+		long current=k/2*total[i]-(s[i]-s[i-k/2])-k/2*(k/2+1)/2; //calculate the distances to combine all left k/2 1s with the median value
+		current+=s[i+1+(k-1)/2]-s[i+1]-(k-1)/2*total[i]-(k-1)/2*((k-1)/2+1)/2; //to calculate the distances to combine all right (k-1)/2 1s with the median value
+		result=min(result,current);
+	}
+	return result;
+
+        ////
+    }
+};

dab4f60c3641cb4d864137e3266e3dd8d372dac1
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-10-22 20:46:18 +0530

    Time: 11 ms (95.02%), Space: 10.6 MB (95.56%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -18,27 +18,38 @@ public:
                 }
             }
         }
-        string ans = "";
-        int i = m, j = n;
-        while(i>0 and j>0){
-            if(str1[i-1]==str2[j-1]){
-                ans.push_back(str1[i-1]);
-                   i--;j--;
-            }else{
-                 if(t[i-1][j] > t[i][j-1]){
-                        ans.push_back(str1[i-1]);
-                        i--;
-                    }
-                else{
-                    ans.push_back(str2[j-1]);
-                     j--;
-                 }
+       
+        string ans = "" ;
+        int i = m , j = n ;
+        
+        while(i > 0 && j > 0){
+            if(str1[i-1] == str2[j-1]){
+                ans.push_back(str1[i-1]) ;
+                i--;j--;
+            }
+            else{
+            if(t[i][j-1] > t[i-1][j]) {
+                ans.push_back(str2[j-1]) ;
+                 j-- ;
+            }
+                   
+                else
+                {
+                    ans.push_back(str1[i-1]) ;
+                    i-- ;
+                }
+                
             }
         }
-        while(i>0) {ans.push_back(str1[i-1]);i--;}
-        while(j>0) {ans.push_back(str2[j-1]);j--;}
-        reverse(ans.begin(), ans.end());
-        return ans;
+        
+        while(i>0 ) {ans.push_back(str1[i-1]); i-- ;}
+        while(j>0 ) {ans.push_back(str2[j-1]); j-- ;}
+        
+        reverse(ans.begin(),ans.end()) ;
+        
+        return ans ;
+        
+        
 
         
     }

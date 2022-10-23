f57cc7e0cf770e10a1f0b03222bb9a15aaa9a147
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-10-23 14:56:32 +0530

    Time: 132 ms (17.26%), Space: 14.2 MB (35.77%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.cpp
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
     string largestPalindromic(string num) {
-        vector<int>freqArr(10,0); 
-         for(char i:num)freqArr[i - '0']++;
         
-         string front = "", back = "";
+    unordered_map<char,int> m ;
+    
+        for(char c : num){
+            m[c]++ ;
+        }
         
-        for(int i = 9;i>=0;i--) 
-        {
-           
-            if(i == 0 && front.empty())
-                continue;
+        string front  = "", back = "" ;
         
-            while(freqArr[i] > 1) 
-            {
-                // Inserting the characters in both the strings
-                
-                front += to_string(i);
-                back += to_string(i);
-                freqArr[i] -= 2;
+        for(char i = '9' ; i >= '0' ;i--){
+            
+            if(front == "" && i == '0')
+                continue ;
+            
+            if(m.find(i) != m.end())
+            while(m[i] > 1){
+                front.push_back(i) ;
+                back.push_back(i) ;
+                m[i] -= 2 ;
             }
         }
         
-         for(int i = 9;i>=0;i--)
-        {
-            if(freqArr[i]) 
-            {
-                front += to_string(i);
-                break;
-            }
+        for(char i = '9' ; i >= '0' ;i--){
+            
+             if(m.find(i) != m.end()){
+                 if(m[i]> 0)
+                 {front.push_back(i) ;
+                 break ;}
+             }
+             
         }
         
-         reverse(back.begin(),back.end());
+        reverse(back.begin(), back.end()) ;
         
-        return front + back ;
+        string res = front + back ;
+        
+        return res ;
         
     }
 };

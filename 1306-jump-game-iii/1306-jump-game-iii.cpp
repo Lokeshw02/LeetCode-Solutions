18cfba8d6e70a94a6d75d8045dca869ca5493ecf
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-11-01 12:58:34 +0530

    Time: 97 ms (57.98%), Space: 37 MB (17.31%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,47 +1,26 @@
 class Solution {
 public:
-     int dp[100005];
-    int  solve(vector<int>&a,int idx,int n)
-    {
-        if(idx >= n || idx < 0)
-            return 0;
-        if(a[idx] == 0)
+    int dp[500005];
+    int helper(vector<int>&arr, int start, int n){
+        
+        if(start >= n || start < 0 ) 
+            return 0 ;
+        
+        if(arr[start] == 0)
             return 1;
         
-        if(dp[idx]!=-1)
-            return dp[idx];
-        
-        dp[idx] = 0;
-        
-        int res = solve(a,idx+a[idx],n) || solve(a,idx-a[idx],n);
-        
-         dp[idx] = res;
+        if(dp[start]!= -1 )
+            return dp[start] ;
+         dp[start] = 0;
+       
+        int res = helper(arr,start + arr[start],n) || helper(arr,start-arr[start],n);
+        dp[start] = res;
         return res;
     }
-    
-//     int helper(vector<int> arr, vector<int> dp, int start, int n){
-        
-//         if(start >= n || start < 0 ) 
-//             return 0 ;
-        
-//         if(dp[start] != -1 )
-//             return dp[start] ;
-        
-//         if(arr[start] ==  0)
-//             return dp[start] = 1;
-        
-//         int k = arr[start] ;
-        
-//        if(helper(arr,dp,start + k, n) || helper(arr,dp,start- k, n)) 
-//                 return dp[start] = 1 ;
-      
-//       return dp[start] = 0;
-        
-//     }
     
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size() ;
         memset(dp,-1,sizeof(dp));
-        return solve(arr,start,n);
+        return helper(arr,start,n);
     }
 };

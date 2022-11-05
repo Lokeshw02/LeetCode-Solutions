f3f5f3cb12dbfc086009edceed9bef4ee2935289
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-11-06 02:13:33 +0530

    Time: 15 ms (60.00%), Space: 8.8 MB (71.41%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+     vector<int>parent ;
+    
+    void merge(int u, int v){
+        
+        // int g1 = find(u) ;
+        // int g2 = find(v) ;
+        
+        parent[v] = u ;
+    }
+    
+    int find(int u){
+        
+        while(u != parent[u]){
+            u = parent[u] ;
+        }
+        
+        return u ;
+    }
+    
+    
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        
+        int n = edges.size() ;
+       
+        parent.push_back(0) ;
+        
+        for(int i = 1 ; i <= n ; i++){
+            parent.push_back(i) ;
+        }
+        
+        
+        for(int i = 0 ; i< n ; i++ ){
+            
+            int g1 = find(edges[i][0]) ;
+             int g2 = find(edges[i][1]) ; 
+            
+            if(g1 == g2 ) 
+                return edges[i] ;
+            
+            merge(g1,g2) ;
+        }
+        
+        return {} ;
+     }
+};

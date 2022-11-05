a88381a6fee17f819d60c1b581a8ccc716f92a0c
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-11-06 02:35:45 +0530

    Time: 278 ms (56.09%), Space: 37.9 MB (86.93%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,64 +1,50 @@
-class ds{
-    private :
-    int total ;
-    vector<int>parent; 
-    int num_groups ;
+class Solution {
+public:
+     vector<int>parent ;
     
-    public :
-    
-     ds(int n){
-        total = n ;
-        num_groups = n ;
-        
-        for(int i = 0 ; i < n ; i++){
-            parent.push_back(i) ;
-        }
-    }
-    
-    int find(int u ){
-        while(u != parent[u]){
-            u = parent[u] ;
-        }
-        
-        return u ;
-    }
     
     void merge(int u, int v){
         int g1 = find(u) ;
         int g2 = find(v) ;
         
         parent[g2] = g1 ;
-        num_groups-- ;
     }
     
-    int getnum_groups(){
-        return num_groups ;
+    int find(int u){
+        
+        while(u != parent[u])
+            u = parent[u] ;
+        
+        return u ;
     }
-};
-
-
-
-class Solution {
-public:
+    
+    
     int makeConnected(int n, vector<vector<int>>& connections) {
         
-        ds uf(n) ;
-        int extra_edge = 0, num_of_groups = 0 ;
+       for(int i = 0 ;i <= n ; i++){
+           parent.push_back(i) ;
+       }
+        
+        int extra_edges = 0 , c =0 ;
         
         for(int i = 0 ; i < connections.size() ; i++){
             
-            int g1 = uf.find(connections[i][0]) ;
-            int g2 = uf.find(connections[i][1]) ;
+            int g1 = find( connections[i][0]) ;
+            int g2 = find( connections[i][1]) ; 
             
-            if(g1 == g2 )
-                extra_edge++ ;
-            else
-                uf.merge(g1,g2) ;
-                
+            if(g1 == g2) 
+             extra_edges++ ;
+            else{
+                 merge(g1,g2) ;
+                 c++ ;
+            } 
+               
         }
-        num_of_groups = uf.getnum_groups() ;
         
-        return extra_edge >= num_of_groups -1 ? num_of_groups -1 : -1 ;
+       if(extra_edges >= n -(c+1) )
+           return  n -(c+1) ;
+        
+        return -1 ;
         
     }
 };

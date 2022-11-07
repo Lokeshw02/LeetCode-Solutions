4b8ec16ff901005b31ae71bc41715a288443e94b
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-11-07 13:32:52 +0530

    Time: 314 ms (29.39%), Space: 67.7 MB (54.37%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,15 +4,20 @@ public:
         
        int n = nums.size() ;
         
-        vector<int>dp(n,0 ) ;
+     //   vector<int>dp(n,0 ) ;
         
-        dp[0] = nums[0] ;
+      //  dp[0] = nums[0] ;
+        int prevsum = nums[0] ;
         int ans = nums[0] ;
+        int  currsum = nums[0] ;
         
         for(int i = 1 ;  i < n ; i++){
             
-            dp[i] = max(dp[i-1] + nums[i] , nums[i]) ;
-            ans = max(ans,dp[i]) ;
+             
+            // dp[i] = max(dp[i-1] + nums[i] , nums[i]) ;
+            currsum = max (nums[i], prevsum + nums[i] ) ; 
+            ans = max (currsum, ans) ;
+            prevsum = currsum ;
         }
         
         return ans ;

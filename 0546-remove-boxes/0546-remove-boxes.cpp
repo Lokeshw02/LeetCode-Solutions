391ef51a2ed97bb5c9c5b1c5e391a0e045912046
Lokeshw02 <87263807+Lokeshw02@users.noreply.github.com>
2022-11-08 14:01:07 +0530

    Time: 403 ms (54.13%), Space: 42.7 MB (41.12%) - LeetHub

--- 0546-remove-boxes/0546-remove-boxes.cpp
+++ 0546-remove-boxes/0546-remove-boxes.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    int memo[200][200][200] = {};
-     int dp(vector<int>& boxes, int l, int r, int k) {
-        if (l > r) return 0;
-        if (memo[l][r][k] > 0) return memo[l][r][k];
-        int lOrg = l, kOrg = k;
+    
+    int dp[200][200][200] ={0} ;
+    
+    int helper(vector<int>& boxes, int l, int r , int k) {
 
-        while (l+1 <= r && boxes[l] == boxes[l+1]) { // Increase both `l` and `k` if they have consecutive colors with `boxes[l]`
-            l += 1;
-            k += 1;
-        }
-        int ans = (k+1) * (k+1) + dp(boxes, l+1, r, 0); // Remove all boxes which has the same with `boxes[l]`
-        for (int m = l + 1; m <= r; m++) // Try to merge non-contiguous boxes of the same color together
-            if (boxes[m] == boxes[l])
-                ans = max(ans, dp(boxes, m, r, k+1) + dp(boxes, l+1, m-1, 0));
-        return memo[lOrg][r][kOrg] = ans;
+    if(l>r ) 
+        return 0 ;
+        
+    if(dp[l][r][k] > 0) return dp[l][r][k] ;
+        
+        int lrg = l, krg = k ;
+        
+       while(l+1 <= r && boxes[l] == boxes[l+1]){
+           l++ ;
+           k++ ;
+       }
+        
+       int ans = (k+1)*(k+1) + helper(boxes,l+1,r,0) ;
+        
+       for(int m = l+1; m <= r ; m++){
+           
+           if(boxes[m] == boxes[l]) 
+               ans = max(ans , helper(boxes,m,r,k+1) + helper(boxes,l+1,m-1,0)) ;
+       }
+        
+        return dp[lrg][r][krg] = ans  ;
+
     }
+     
     int removeBoxes(vector<int>& boxes) {
-         return dp(boxes, 0, boxes.size() - 1, 0);
+        return helper(boxes,0,boxes.size() -1 ,0) ;
     }
 };
